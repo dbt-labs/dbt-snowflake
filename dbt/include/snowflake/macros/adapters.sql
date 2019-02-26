e9f462f614a865dc1e18c388a4f5c299f61d7cff
Drew Banin <drew@fishtownanalytics.com>
2019-02-26 13:08:28 -0500

    (fixes #1316) Fix for alter table rename on Snowflake tables, added tests

--- dbt/include/snowflake/macros/adapters.sql
+++ dbt/include/snowflake/macros/adapters.sql
@@ -81,3 +81,10 @@
 {% macro snowflake__current_timestamp() -%}
   convert_timezone('UTC', current_timestamp())
 {%- endmacro %}
+
+
+{% macro snowflake__rename_relation(from_relation, to_relation) -%}
+  {% call statement('rename_relation') -%}
+    alter table {{ from_relation }} rename to {{ to_relation }}
+  {%- endcall %}
+{% endmacro %}
