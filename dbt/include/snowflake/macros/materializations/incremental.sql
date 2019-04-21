591ced922490875c903e9cd01a9511b594a5a428
Bastien Boutonnet <bastien.b1@gmail.com>
2019-04-22 01:27:25 +0200

    fix insert cols call and temp workaround call of snowflake

--- dbt/include/snowflake/macros/materializations/incremental.sql
+++ dbt/include/snowflake/macros/materializations/incremental.sql
@@ -16,18 +16,11 @@
   {%- set exists_as_table = (old_relation is not none and old_relation.is_table) -%}
   {%- set exists_not_as_table = (old_relation is not none and not old_relation.is_table) -%}
 
-  -- FIXME: Double check the operators syntax as non-destrive always has to be false here
-  {%- set should_drop = (full_refresh_mode or exists_not_as_table and not non_destructive_mode) -%}
+
   {%- set force_create_or_replace = full_refresh_mode -%}
 
 
   -- setup
-  {% if old_relation is none -%}
-    -- noop
-  {%- elif should_drop -%}
-    {{ adapter.drop_relation(old_relation) }}
-    {%- set old_relation = none -%}
-  {%- endif %}
 
   {% set source_sql -%}
      {#-- wrap sql in parens to make it a subquery --#}
@@ -57,24 +50,28 @@
           {{ log("Dropping relation " ~ old_relation ~ " because it is a view and this model is a table.") }}
           {{ adapter.drop_relation(old_relation) }}
       {% endif %}
-  
       {# -- now create or replace the table because we're in full-refresh #}
-      {{create_or_replace_table_as(target_relation, source_sql)}}
+      {{ log("Creating or Replacing table") }}
+      {{snowflake__create_or_replace_table_as(target_relation, source_sql)}}
     {%- endcall -%}
 
   {%- else -%}
     {# -- here is the incremental part #}
     {% set dest_columns = adapter.get_columns_in_relation(target_relation) %}
+    {% set dest_cols_csv = dest_columns | map(attribute='quoted') | join(', ') %}
     {%- call statement('main') -%}
+    
       {%- if unique_key is none -%}
-      {# -- if no unique_key is provided run regular insert as Snowflake may complain #}
-        insert into {{ target_relation }} ({{ dest_columns }})
+        {{ log("Incremental Run happening via INSERT") }}
+        {# -- if no unique_key is provided run regular insert as Snowflake may complain #}
+        insert into {{ target_relation }} ({{ dest_cols_csv }})
         (
-          select {{ dest_columns }}
+          select {{ dest_cols_csv }}
           from {{ source_sql }}
         );
       {%- else -%}
-      {# -- use merge if a unique key is provided #}
+        {# -- use merge if a unique key is provided #}
+        {{ log("Incremental Run happening via MERGE") }}
         {{ get_merge_sql(target_relation, source_sql, unique_key, dest_columns) }}
       {%- endif -%}
     {% endcall %}
@@ -85,9 +82,6 @@
 
   -- `COMMIT` happens here
   {{ adapter.commit() }}
-
-{# -- FIXME: There doesn't seem to be any backup relation created here. Need to check whether we 
--- should have one #}
 
   {{ run_hooks(post_hooks, inside_transaction=False) }}
 
