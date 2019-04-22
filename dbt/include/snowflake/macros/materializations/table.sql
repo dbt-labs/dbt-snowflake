41035c32f9dbea56d3b9a34babe587d46a6d1d13
Bastien Boutonnet <bastien.b1@gmail.com>
2019-04-22 11:06:58 +0200

    make default create or replace macro to allow snowflake adapter pick it up

--- dbt/include/snowflake/macros/materializations/table.sql
+++ dbt/include/snowflake/macros/materializations/table.sql
@@ -64,7 +64,7 @@
     {% endif %}
     -- 
     {{ log("Using snowflake create or replace method.") }}
-    {{snowflake__create_or_replace_table_as(target_relation, sql)}}
+    {{create_or_replace_table_as(target_relation, sql)}}
   {%- endcall %}
 
   -- skiping all previous renames here since they are not needed in Snowflake
