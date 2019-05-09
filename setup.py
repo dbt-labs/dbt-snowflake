987a49c2c36805253c384f624fce62bd4ccb6c19
Drew Banin <drew@fishtownanalytics.com>
2019-05-09 15:58:44 -0400

    update plugin readmes

--- setup.py
+++ setup.py
@@ -6,12 +6,16 @@ package_name = "dbt-snowflake"
 package_version = "0.13.1a2"
 description = """The snowflake adapter plugin for dbt (data build tool)"""
 
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name=package_name,
     version=package_version,
     description=description,
     long_description=description,
+    long_description_content_type='text/markdown',
     author="Fishtown Analytics",
     author_email="info@fishtownanalytics.com",
     url="https://github.com/fishtown-analytics/dbt",
