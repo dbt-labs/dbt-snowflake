028026084ff029e535cec8834ea016959d9a13d4
Jacob Beck <jake@fishtownanalytics.com>
2019-11-04 09:24:31 -0700

    add some arbitrary-ish classifiers (importantly: the versions we support)

--- setup.py
+++ setup.py
@@ -36,4 +36,17 @@ setup(
         'urllib3<1.25.0',
     ],
     zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'License :: OSI Approved :: Apache Software License',
+
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
