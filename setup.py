16223a7d209fe12e8c168b7dd20f2e0b514d6c9d
Jacob Beck <jake@fishtownanalytics.com>
2020-02-21 09:36:43 -0700

    Dependency revs/bounding
    remove jinja._compat calls
    set an upper bound on jinja anyway
    set bound on idna/requests to match snowlfake
    update snowflake-connector-python

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     },
     install_requires=[
         'dbt-core=={}'.format(package_version),
-        'snowflake-connector-python==2.2.0',
+        'snowflake-connector-python==2.2.1',
         'azure-common<2.0.0',
         'azure-storage-blob<12.0.0',
         'urllib3>=1.20,<1.26.0',
