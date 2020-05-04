c2fa4748ad12496e8168267beb4d2091aa57683c
Jacob Beck <jake@fishtownanalytics.com>
2020-05-04 14:59:39 -0600

    Bump version: 0.17.0a1 → 0.17.0b1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except ImportError:
 
 
 package_name = "dbt-snowflake"
-package_version = "0.17.0a1"
+package_version = "0.17.0b1"
 description = """The snowflake adapter plugin for dbt (data build tool)"""
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
