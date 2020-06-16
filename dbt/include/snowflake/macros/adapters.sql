97326bb39d0b115115ee94bc89dca05ca362268b
Dan Rubin <dmrubin3@gmail.com>
2020-06-16 18:43:26 -0400

    add snowflake macros to support fetching current session query tag, updating the session query tag, and reverting to the old query tag

--- dbt/include/snowflake/macros/adapters.sql
+++ dbt/include/snowflake/macros/adapters.sql
@@ -160,3 +160,33 @@
         {{ column_name }} COMMENT $${{ column_dict[column_name]['description'] | replace('$', '[$]') }}$$ {{ ',' if not loop.last else ';' }}
     {% endfor %}
 {% endmacro %}
+
+
+{% macro get_current_query_tag() -%}
+  {{ return(run_query("show parameters like 'query_tag' in session").rows[0]['value']) }}
+{% endmacro %}
+
+
+{% macro set_query_tag() -%}
+  {% set new_query_tag = config.get('query_tag', none) %}
+  {% if new_query_tag %}
+    {% set original_query_tag = get_current_query_tag() %}
+    {{ log("Setting query_tag to '" ~ new_query_tag ~ "'. Will reset to '" ~ original_query_tag ~ "' after materialization.") }}
+    {% do run_query("alter session set query_tag = '{}'".format(new_query_tag)) %}
+    {{ return(original_query_tag)}}
+  {% endif %}
+  {{ return(none)}}
+{% endmacro %}
+
+{% macro unset_query_tag(original_query_tag) -%}
+  {% set new_query_tag = config.get('query_tag', none) %}
+  {% if new_query_tag %}
+    {% if original_query_tag %}
+      {{ log("Resetting query_tag to '" ~ original_query_tag ~ "'.") }}
+      {% do run_query("alter session set query_tag = '{}'".format(original_query_tag)) %}
+    {% else %}
+      {{ log("No original query_tag, unsetting parameter.") }}
+      {% do run_query("alter session unset query_tag") %}
+    {% endif %}
+  {% endif %}
+{% endmacro %} 
