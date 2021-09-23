2b561c894acda7d41cb6d16cf75e379bc0f8f3e7
Jeremy Cohen <jeremy@fishtownanalytics.com>
2021-09-23 08:49:52 -0700

    Fix batching for Snowflake seeds >10k rows (#3942)
    * Call get_batch_size in snowflake__load_csv_rows
    
    * Git ignore big csv. Update changelog

--- dbt/include/snowflake/macros/materializations/seed.sql
+++ dbt/include/snowflake/macros/materializations/seed.sql
@@ -1,4 +1,5 @@
 {% macro snowflake__load_csv_rows(model, agate_table) %}
+    {% set batch_size = get_batch_size() %}
     {% set cols_sql = get_seed_column_quoted_csv(model, agate_table.column_names) %}
     {% set bindings = [] %}
 
