d0002e0177ec09a96de3a9b4a9728c023c3f8bb5
Mila Page <versusfacit@users.noreply.github.com>
2022-02-17 10:59:24 -0800

    Remove zombie test file that is no longer needed for tests.

--- tests/integration/incremental_unique_id_test/seeds/extra_seattle_row.sql
+++
@@ -1,9 +0,0 @@
--- insert new row, which should be in incremental model with
---  first three columns unique
-insert into {schema}.seed
-    (state, county, city, last_visit_date)
-values ('WA','King','Seattle','2022-02-10');
-
-insert into {schema}.seed
-    (state, county, city, last_visit_date)
-values ('WA','King','Seattle','2022-02-10');
