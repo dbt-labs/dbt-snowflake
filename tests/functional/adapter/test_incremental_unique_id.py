5ca58d5469f4d37d126856b129482abcc671f03e
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2022-04-01 11:17:49 -0500

    adding tests for delete+insert and changing pointer to get tests passing in ci

--- tests/functional/adapter/test_incremental_unique_id.py
+++ tests/functional/adapter/test_incremental_unique_id.py
@@ -1,5 +1,14 @@
+import pytest
 from dbt.tests.adapter.incremental.test_incremental_unique_id import BaseIncrementalUniqueKey
 
 
 class TestUniqueKeySnowflake(BaseIncrementalUniqueKey):
     pass
+
+
+class TestUniqueKeyDeleteInsertSnowflake(BaseIncrementalUniqueKey):
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {
+            "models": { "+incremental_strategy": "delete+insert" }
+        }
