4e96fe45aaaa2a9892c4bc063a465b2888693ba6
Chenyu Li <chenyu.li@dbtlabs.com>
2022-04-08 14:37:09 -0600

    convert adapter method test

--- tests/integration/adapter_methods_test/macros/rename_named_relation.sql
+++
@@ -1,6 +0,0 @@
--- Macro to rename a relation
-{% macro rename_named_relation(from_name, to_name) %}
-{%- set from_relation = api.Relation.create(database=target.database, schema=target.schema, identifier=from_name, type='table') -%}
-{%- set to_relation = api.Relation.create(database=target.database, schema=target.schema, identifier=to_name, type='table') -%}
-{% do adapter.rename_relation(from_relation, to_relation) %}
-{% endmacro %}
