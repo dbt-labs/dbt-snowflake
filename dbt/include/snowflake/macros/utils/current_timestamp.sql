ef014dbd44f8352784289dcf27b4a4e2e3e10536
Doug Beatty <doug.beatty@dbtlabs.com>
2022-05-25 11:11:32 -0600

    Switch namespace from `dbt_utils` to `dbt`

--- dbt/include/snowflake/macros/utils/current_timestamp.sql
+++ dbt/include/snowflake/macros/utils/current_timestamp.sql
@@ -1,3 +1,3 @@
 {% macro snowflake__current_timestamp_in_utc() %}
-    convert_timezone('UTC', {{dbt_utils.current_timestamp()}})::{{dbt_utils.type_timestamp()}}
+    convert_timezone('UTC', {{dbt.current_timestamp()}})::{{dbt.type_timestamp()}}
 {% endmacro %}
