b3b782a129aa1b33559c13c305f44518544e08f0
leahtessem <leah.tessem@gmail.com>
2022-06-23 14:36:10 -0500

    update

--- dbt/adapters/snowflake/relation.py
+++ dbt/adapters/snowflake/relation.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from dbt.adapters.base.relation import BaseRelation, Policy
-
+from dbt.dataclass_schema import StrEnum
 
 @dataclass
 class SnowflakeQuotePolicy(Policy):
@@ -9,6 +9,22 @@ class SnowflakeQuotePolicy(Policy):
     identifier: bool = False
 
 
+class RelationType(StrEnum):
+    Table = "table"
+    View = "view"
+    CTE = "cte"
+    MaterializedView = "materialized view"
+    External = "external"
+
+
 @dataclass(frozen=True, eq=False, repr=False)
 class SnowflakeRelation(BaseRelation):
     quote_policy: SnowflakeQuotePolicy = SnowflakeQuotePolicy()
+
+    @property
+    def is_materializedview(self) -> bool:
+        return self.type == RelationType.MaterializedView
+
+    @classproperty
+    def MaterializedView(cls) -> str:
+        return str(RelationType.MaterializedView)
