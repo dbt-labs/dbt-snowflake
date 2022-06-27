b518d288eefa1ae2b929162382348bf24d7f4628
leahtessem <leah.tessem@gmail.com>
2022-06-27 15:29:42 -0500

    Revert "dependency test"
    This reverts commit e71e4ce9b6f07b037f9045c7380eae3261f7d171.

--- tests/integration/snowflake_view_dependency_test/models/dependent_model.sql
+++ tests/integration/snowflake_view_dependency_test/models/dependent_model.sql
@@ -1,10 +1,8 @@
 
 {% if var('dependent_type', 'view') == 'view' %}
     {{ config(materialized='view') }}
-{% elif var('dependent_type') == 'materializedview' %}
-    {{ config(materialized='materializedview') }}
 {% else %}
-{{ config(materialized='table') }}
+    {{ config(materialized='table') }}
 {% endif %}
 
 select * from {{ ref('base_table') }}
