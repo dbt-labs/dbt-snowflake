e71e4ce9b6f07b037f9045c7380eae3261f7d171
leahtessem <leah.tessem@gmail.com>
2022-06-27 15:22:33 -0500

    dependency test

--- tests/integration/snowflake_view_dependency_test/models/dependent_model.sql
+++ tests/integration/snowflake_view_dependency_test/models/dependent_model.sql
@@ -1,8 +1,10 @@
 
 {% if var('dependent_type', 'view') == 'view' %}
     {{ config(materialized='view') }}
+{% elif var('dependent_type') == 'materializedview' %}
+    {{ config(materialized='materializedview') }}
 {% else %}
-    {{ config(materialized='table') }}
+{{ config(materialized='table') }}
 {% endif %}
 
 select * from {{ ref('base_table') }}
