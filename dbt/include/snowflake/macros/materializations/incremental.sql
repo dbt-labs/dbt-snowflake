148aca4cf53a2c6b7b7a80b32747b16ab47b30ee
leahtessem <leah.tessem@gmail.com>
2022-06-27 10:09:44 -0500

    handle mvs for incremental

--- dbt/include/snowflake/macros/materializations/incremental.sql
+++ dbt/include/snowflake/macros/materializations/incremental.sql
@@ -44,7 +44,7 @@
   {% if existing_relation is none %}
     {% set build_sql = create_table_as(False, target_relation, sql) %}
 
-  {% elif existing_relation.is_view %}
+  {% elif existing_relation.is_view or existing_relation.is_materializedview %}
     {#-- Can't overwrite a view with a table - we must drop --#}
     {{ log("Dropping relation " ~ target_relation ~ " because it is a view and this model is a table.") }}
     {% do adapter.drop_relation(existing_relation) %}
