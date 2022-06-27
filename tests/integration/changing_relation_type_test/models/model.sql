415f88e3681de1448ea99b72c328b6f7964c2578
leahtessem <leah.tessem@gmail.com>
2022-06-27 10:09:53 -0500

    add MV to test

--- tests/integration/changing_relation_type_test/models/model.sql
+++ tests/integration/changing_relation_type_test/models/model.sql
@@ -1,8 +1,11 @@
-
 {{ config(materialized=var('materialized')) }}
 
 select '{{ var("materialized") }}' as materialization
 
 {% if var('materialized') == 'incremental' and is_incremental() %}
     where 'abc' != (select max(materialization) from {{ this }})
+{% endif %}
+
+{% if var('materialized') ==  'materializedview' %}
+from change_relation_type_tbl
 {% endif %}
--- tests/integration/changing_relation_type_test/test_changing_relation_type.py
+++ tests/integration/changing_relation_type_test/test_changing_relation_type.py
@@ -16,6 +16,7 @@ class TestChangingRelationType(DBTIntegrationTest):
         return self.dir("models")
 
     def swap_types_and_test(self):
+        self.run_sql_file('mv_setup_seed.sql')
         # test that dbt is able to do intelligent things when changing
         # between materializations that create tables and views.
 
@@ -37,6 +38,30 @@ class TestChangingRelationType(DBTIntegrationTest):
 
         results = self.run_dbt(['run', '--vars', 'materialized: view'])
         self.assertEqual(results[0].node.config.materialized, 'view')
+        self.assertEqual(len(results),  1)
+
+        results = self.run_dbt(['run', '--vars', 'materialized: materializedview'])
+        self.assertEqual(results[0].node.config.materialized, 'materializedview')
+        self.assertEqual(len(results),  1)
+
+        results = self.run_dbt(['run', '--vars', 'materialized: view'])
+        self.assertEqual(results[0].node.config.materialized, 'view')
+        self.assertEqual(len(results),  1)
+
+        results = self.run_dbt(['run', '--vars', 'materialized: materializedview'])
+        self.assertEqual(results[0].node.config.materialized, 'materializedview')
+        self.assertEqual(len(results),  1)
+
+        results = self.run_dbt(['run', '--vars', 'materialized: table'])
+        self.assertEqual(results[0].node.config.materialized, 'table')
+        self.assertEqual(len(results),  1)
+
+        results = self.run_dbt(['run', '--vars', 'materialized: materializedview'])
+        self.assertEqual(results[0].node.config.materialized, 'materializedview')
+        self.assertEqual(len(results),  1)
+
+        results = self.run_dbt(['run', '--vars', 'materialized: incremental'])
+        self.assertEqual(results[0].node.config.materialized, 'incremental')
         self.assertEqual(len(results),  1)
 
     @use_profile("snowflake")
