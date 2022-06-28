754315a58bea515d87feb415bbc2c387a9ddc41d
leahtessem <leah.tessem@gmail.com>
2022-06-28 09:43:16 -0500

    remove pre hooks

--- dbt/include/snowflake/macros/materializations/view.sql
+++ dbt/include/snowflake/macros/materializations/view.sql
@@ -1,7 +1,6 @@
 {% materialization view, adapter='snowflake' -%}
 
     {% set original_query_tag = set_query_tag() %}
-    {% set target_relation = this.incorporate(type='view') %}
     {%- set identifier = model['alias'] -%}
     {%- set old_relation = adapter.get_relation(database=database, schema=schema, identifier=identifier) -%}
 
@@ -13,7 +12,7 @@
 
     {% set to_return = create_or_replace_view() %}
 
-    {{ run_hooks(pre_hooks) }}
+    {% set target_relation = this.incorporate(type='view') %}
 
     {% do persist_docs(target_relation, model, for_columns=false) %}
 
