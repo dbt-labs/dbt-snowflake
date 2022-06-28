8d0149fbd64f5d0a8c51da5385e6b90640e7cc83
leahtessem <leah.tessem@gmail.com>
2022-06-28 08:47:04 -0500

    whitespace

--- dbt/include/snowflake/macros/materializations/view.sql
+++ dbt/include/snowflake/macros/materializations/view.sql
@@ -1,20 +1,20 @@
 {% materialization view, adapter='snowflake' -%}
 
     {% set original_query_tag = set_query_tag() %}
-    {% set to_return = create_or_replace_view() %}
-    {%- set identifier = model['alias'] -%}
     {% set target_relation = this.incorporate(type='view') %}
+    {%- set identifier = model['alias'] -%}
     {%- set old_relation = adapter.get_relation(database=database, schema=schema, identifier=identifier) -%}
+
+    {{ run_hooks(pre_hooks) }}
+
+    {% do persist_docs(target_relation, model, for_columns=false) %}
 
     -- if object exists already as MV, drop it before create_or_replace_view() macro
     {%- if old_relation is not none and old_relation.is_materializedview -%}
     {{ handle_existing_table(should_full_refresh(), old_relation) }}
     {%- endif -%}
 
-    {{ run_hooks(pre_hooks) }}
-
-    {% do persist_docs(target_relation, model, for_columns=false) %}
-
+    {% set to_return = create_or_replace_view() %}
 
     {% do return(to_return) %}
 
