eabfef2db51e8f0622ac72031909ed101cbdaf25
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2022-07-08 13:14:42 -0500

    push up addition of more tests pr

--- tests/functional/adapter/test_grants.py
+++ tests/functional/adapter/test_grants.py
@@ -1,5 +1,31 @@
-from dbt.tests.adapter.grants.test_models import TestModelGrants
+from dbt.tests.adapter.grants.base_grants import BaseGrants
+from dbt.tests.adapter.grants.test_incremental_grants import BaseIncrementalGrants
+from dbt.tests.adapter.grants.test_invalid_grants import BaseInvalidGrants
+from dbt.tests.adapter.grants.test_model_grants import BaseModelGrants
+# to do: from dbt.tests.adapter.grants.test_revoke_all import
+from dbt.tests.adapter.grants.test_seed_grants import BaseSeedGrants
+from dbt.tests.adapter.grants.test_snapshot_grants import BaseSnapshotGrants
 
 
-class TestModelGrantsSnowflake(TestModelGrants):
+class TestGrantsSnowflake(BaseGrants):
+    pass
+
+
+class TestIncrementalGrantsSnowflake(BaseIncrementalGrants):
+    pass
+
+
+class TestInvalidGrantsSnowflake(BaseInvalidGrants):
+    pass
+
+
+class TestModelGrantsSnowflake(BaseModelGrants):
+    pass
+
+
+class TestSeedGrantsSnowflake(BaseSeedGrants):
+    pass
+
+
+class TestSnapshotGrants(BaseSnapshotGrants):
     pass
