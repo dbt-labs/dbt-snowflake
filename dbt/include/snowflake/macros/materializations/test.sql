29cce4637fc29f2fea34e39bd605c4a2bf39a9d0
Matt Winkler <matt.winkler@fishtownanalytics.com>
2022-07-22 11:02:04 -0600

    set adapter to snowflake

--- dbt/include/snowflake/macros/materializations/test.sql
+++ dbt/include/snowflake/macros/materializations/test.sql
@@ -1,4 +1,4 @@
-{%- materialization test, default -%}
+{%- materialization test, adapter='snowflake' -%}
 
   {% set original_query_tag = set_query_tag() %}
   {% set relations = materialization_test_default() %}
