c3f080683f1975afbdad9b3622c4d3cdad8c058f
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2022-09-09 01:30:01 -0500

    change how to update seed file based on file path, delete uneeded files, change to run_dbt over run_dbt_and_capture as we don't need to pass output

--- tests/functional/adapter/test_concurrency.py
+++ tests/functional/adapter/test_concurrency.py
@@ -4,7 +4,6 @@ from dbt.tests.util import (
     run_dbt,
     check_relations_equal,
     run_dbt_and_capture,
-    run_sql_with_adapter,
     )
 from dbt.tests.adapter.concurrency.test_concurrency import BaseConcurrency
 
@@ -16,9 +15,10 @@ class TestConncurenncySnowflake(BaseConcurrency):
         check_relations_equal(project.adapter, ["SEED", "DEP"])
         check_relations_equal(project.adapter, ["SEED", "TABLE_A"])
         check_relations_equal(project.adapter, ["SEED", "TABLE_B"])
-        run_sql_with_adapter(project.test_data_dir / Path("update.sql"))
 
-        results = run_dbt_and_capture(["run"], expect_pass=False)
+        project.run_sql_file(project.project_root / Path("seeds/update.sql"))
+
+        results = run_dbt(["run"], expect_pass=False)
         assert len(results) == 7
 
         check_relations_equal(project.adapter, ["SEED", "VIEW_MODEL"])
