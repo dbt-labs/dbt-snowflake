c01d3a99062342f44455ab18ea23c02ca5d1a21b
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2022-09-12 14:10:49 -0500

    convert test to run off csv

--- tests/functional/adapter/test_ephemeral.py
+++ tests/functional/adapter/test_ephemeral.py
@@ -5,6 +5,7 @@ from dbt.tests.util import run_dbt, check_relations_equal
 class TestEphemeralMultiSnowflake(BaseEphemeralMulti):
 
     def test_ephemeral_multi_snowflake(self, project):
+        run_dbt(["seed"])
         results = run_dbt(["run"])
         assert len(results) == 3
         check_relations_equal(project.adapter, ["SEED", "DEPENDENT", "DOUBLE_DEPENDENT", "SUPER_DEPENDENT"])
