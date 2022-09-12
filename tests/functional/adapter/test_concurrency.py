9000a59c4df920ec505c805aaa13d85d1498e891
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2022-09-12 17:13:48 -0500

    moving test to rely on csv file, throwing syntax error currently

--- tests/functional/adapter/test_concurrency.py
+++ tests/functional/adapter/test_concurrency.py
@@ -2,25 +2,23 @@ import pytest
 from pathlib import Path
 from dbt.tests.util import (
     run_dbt,
-    check_relations_equal,
-    run_dbt_and_capture,
+    check_relations_equal
     )
 from dbt.tests.adapter.concurrency.test_concurrency import BaseConcurrency
 
 class TestConncurenncySnowflake(BaseConcurrency):
     def test_conncurrency_snowflake(self, project):
-        results = run_dbt(['run'], expect_pass=False)
+        run_dbt(["seed", "--select", "seed"])
+        results = run_dbt(["run"], expect_pass=False)
         assert len(results) == 7
         check_relations_equal(project.adapter, ["SEED", "VIEW_MODEL"])
         check_relations_equal(project.adapter, ["SEED", "DEP"])
         check_relations_equal(project.adapter, ["SEED", "TABLE_A"])
         check_relations_equal(project.adapter, ["SEED", "TABLE_B"])
 
-        project.run_sql_file(project.project_root / Path("seeds/update.sql"))
-
+        run_dbt(["seed", "--select", "update"])
         results = run_dbt(["run"], expect_pass=False)
         assert len(results) == 7
-
         check_relations_equal(project.adapter, ["SEED", "VIEW_MODEL"])
         check_relations_equal(project.adapter, ["SEED", "DEP"])
         check_relations_equal(project.adapter, ["SEED", "TABLE_A"])
