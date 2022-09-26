dbcb1cf40a0b55b3a712386e6f9afb11aacb4fe9
Doug Beatty <44704949+dbeatty10@users.noreply.github.com>
2022-09-26 14:28:49 -0600

    Tests for `current_timestamp` (#275)
    * Test failure will confirm that it is not a timezone-naive data type
    
    * Temporarily update dev-requirements.text
    
    * Tests for current_timestamp
    
    * Restore original dev-requirements.txt

--- tests/functional/adapter/utils/test_utils.py
+++ tests/functional/adapter/utils/test_utils.py
@@ -4,6 +4,7 @@ from dbt.tests.adapter.utils.test_any_value import BaseAnyValue
 from dbt.tests.adapter.utils.test_bool_or import BaseBoolOr
 from dbt.tests.adapter.utils.test_cast_bool_to_text import BaseCastBoolToText
 from dbt.tests.adapter.utils.test_concat import BaseConcat
+from dbt.tests.adapter.utils.test_current_timestamp import BaseCurrentTimestampAware
 from dbt.tests.adapter.utils.test_dateadd import BaseDateAdd
 from dbt.tests.adapter.utils.test_datediff import BaseDateDiff
 from dbt.tests.adapter.utils.test_date_trunc import BaseDateTrunc
@@ -36,6 +37,11 @@ class TestCastBoolToText(BaseCastBoolToText):
 
 
 class TestConcat(BaseConcat):
+    pass
+
+
+# Use either BaseCurrentTimestampAware or BaseCurrentTimestampNaive but not both
+class TestCurrentTimestamp(BaseCurrentTimestampAware):
     pass
 
 
