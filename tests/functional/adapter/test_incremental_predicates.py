dd6ef832a57fc26f20c580f1cfd1e586106bd957
Dave Connors <dave.connors@fishtownanalytics.com>
2022-11-04 13:23:18 -0500

    add functional tests and pin branch for CI checks

---
+++ tests/functional/adapter/test_incremental_predicates.py
@@ -0,0 +1,19 @@
+import pytest
+from dbt.tests.adapter.incremental.test_incremental_predicates import BaseIncrementalPredicates
+
+
+class TestIncrementalPredicatesDeleteInsertSnowflake(BaseIncrementalPredicates):
+    pass
+
+
+class TestIncrementalPredicatesMergeSnowflake(BaseIncrementalPredicates):
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {
+            "models": { 
+                "+incremental_predicates": [
+                    "dbt_internal_dest.id != 2"
+                    ],
+                "+incremental_strategy": "merge"
+            }
+        }
