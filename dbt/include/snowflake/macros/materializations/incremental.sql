1026161e423fb81d146a4621466db2b9741a6490
Dave Connors <dave.connors@fishtownanalytics.com>
2022-11-28 09:38:58 -0600

    update strategy arg dict, added predicates to proper macros

--- dbt/include/snowflake/macros/materializations/incremental.sql
+++ dbt/include/snowflake/macros/materializations/incremental.sql
@@ -80,7 +80,7 @@
     {#-- Get the incremental_strategy, the macro to use for the strategy, and build the sql --#}
     {% set incremental_predicates = config.get('incremental_predicates', none) %}
     {% set strategy_sql_macro_func = adapter.get_incremental_strategy_macro(context, incremental_strategy) %}
-    {% set strategy_arg_dict = ({'target_relation': target_relation, 'temp_relation': tmp_relation, 'unique_key': unique_key, 'dest_columns': dest_columns, 'predicates': incremental_predicates }) %}
+    {% set strategy_arg_dict = ({'target_relation': target_relation, 'temp_relation': tmp_relation, 'unique_key': unique_key, 'dest_columns': dest_columns, 'incremental_predicates': incremental_predicates }) %}
 
     {%- call statement('main') -%}
       {{ strategy_sql_macro_func(strategy_arg_dict) }}
