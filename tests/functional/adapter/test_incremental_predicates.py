3655c24f2ed28ce259e1a7a5f17c97bc6a268ce4
Dave Connors <dave.connors@fishtownanalytics.com>
2022-12-01 10:16:38 -0600

    handle predicates config, add tests

--- tests/functional/adapter/test_incremental_predicates.py
+++ tests/functional/adapter/test_incremental_predicates.py
@@ -6,12 +6,38 @@ class TestIncrementalPredicatesDeleteInsertSnowflake(BaseIncrementalPredicates):
     pass
 
 
+class TestPredicatesDeleteInsertSnowflake(BaseIncrementalPredicates):
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {
+            "models": {
+                "+predicates": [
+                    "id != 2"
+                ],
+                "+incremental_strategy": "delete+insert"
+            }
+        }
+
+
 class TestIncrementalPredicatesMergeSnowflake(BaseIncrementalPredicates):
     @pytest.fixture(scope="class")
     def project_config_update(self):
         return {
             "models": { 
                 "+incremental_predicates": [
+                    "dbt_internal_dest.id != 2"
+                ],
+                "+incremental_strategy": "merge"
+            }
+        }
+
+
+class TestPredicatesMergeSnowflake(BaseIncrementalPredicates):
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {
+            "models": { 
+                "+predicates": [
                     "dbt_internal_dest.id != 2"
                 ],
                 "+incremental_strategy": "merge"
