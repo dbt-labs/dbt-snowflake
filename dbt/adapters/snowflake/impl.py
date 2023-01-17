0fdb0fc11a6cf2272beb3475e9b6f33bd16bf2fe
Matthew McKnight <91097623+McKnight-42@users.noreply.github.com>
2023-01-17 15:33:51 -0600

    [CT-1507] create config for incremental models to use temp tables or views to fix regression in perf (#389)
    * init push for pr to work on regression in temp tables vs views for dbt-snowflake
    
    * adding additional table selection logic, functionality works locally based off snowflake query history
    
    * extend condition logic to smaller more readable elif's take into account some lost table options
    
    * add changie
    
    * breaking out language selection for more readability, reorganizing conditional flow
    
    * typo fix
    
    * spacing fix
    
    * change messaging for incremental model strategy selection
    
    * add direct reference to delete+insert
    
    * changing comment formatting and structure

--- dbt/adapters/snowflake/impl.py
+++ dbt/adapters/snowflake/impl.py
@@ -27,6 +27,7 @@ class SnowflakeConfig(AdapterConfig):
     copy_grants: Optional[bool] = None
     snowflake_warehouse: Optional[str] = None
     query_tag: Optional[str] = None
+    merge_tmp_relation_type: Optional[str] = None
     merge_update_columns: Optional[str] = None
 
 
