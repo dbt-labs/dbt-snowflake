4a3ff20057e8cc10213bb710328bd9f8f7be40cd
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2023-01-18 15:08:01 -0600

    comment changes

--- dbt/include/snowflake/macros/materializations/incremental.sql
+++ dbt/include/snowflake/macros/materializations/incremental.sql
@@ -18,11 +18,9 @@
        With the default strategy of merge, the user may choose between a temporary
        table and view (defaulting to view).
 
-       The append strategy can use a view because it will run a single INSERT statement
-       when the unique_key is none.
-
        When unique_key is none, the delete+insert strategy can use a view beacuse a
        single INSERT statement is run with no DELETES as part of the statement.
+       Otherwise, play it safe by using a temporary table.
   #} */
 
   {% if language == "python" and tmp_relation_type is not none %}
