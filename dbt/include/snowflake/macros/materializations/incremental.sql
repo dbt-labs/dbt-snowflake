aa8d20fdfe14ae6ed8d51df16f42781483354b83
Matthew McKnight <91097623+McKnight-42@users.noreply.github.com>
2023-01-19 12:18:22 -0600

    [CT-1507] part 2: Expand configuration to all strategies (#403)
    * init push for pr to work on regression in temp tables vs views for dbt-snowflake
    
    * adding additional table selection logic, functionality works locally based off snowflake query history
    
    * extend condition logic to smaller more readable elif's take into account some lost table options
    
    * add changie
    
    * breaking out language selection for more readability, reorganizing conditional flow
    
    * typo fix
    
    * spacing fix
    
    * change messaging for incremental model strategy selection
    
    * add direct reference to delete+insert
    
    * changing comment formatting and structure
    
    * expand tmp_relation_type to be configurable for all incremental options
    
    * re organize conditionl logic, add leading condtional for exceptions
    
    * typo
    
    * seperate if statements
    
    * fix exception blocking
    
    * seperate execptions condtional into two small condtionals to prevent a always case
    
    * change conditional logic based on language
    
    * change delete+insert exception logic, and messages for both exceptions
    
    * change condition for more jinja language
    
    * remove default value assigned to tmp_relation_type, change delete+insert excpetion to only kick off if view is assigned and not unique_key not none
    
    * reword python exception
    
    * minor comment change, revert some condtional logic (need to deal with none) value
    
    * re add assignment of config
    
    * comment changes
    
    * put back low level part about append strategy
    
    * minor change to workign for excpetion
    
    * add changelog entry
    
    * fix previous changelog entry
    
    * change change log entry, remove some config from integration test

--- dbt/include/snowflake/macros/materializations/incremental.sql
+++ dbt/include/snowflake/macros/materializations/incremental.sql
@@ -1,5 +1,5 @@
 {% macro dbt_snowflake_get_tmp_relation_type(strategy, unique_key, language) %}
-  {%- set merge_tmp_relation_type = config.get('merge_tmp_relation_type', default="view") -%}
+{%- set tmp_relation_type = config.get('tmp_relation_type') -%}
   /* {#
        High-level principles:
        If we are running multiple statements (DELETE + INSERT),
@@ -11,20 +11,41 @@
        for (presumably) faster overall incremental processing.
 
        Low-level specifics:
+       If an invalid option is specified, then we will raise an
+       excpetion with corresponding message.
+
        Languages other than SQL (like Python) will use a temporary table.
        With the default strategy of merge, the user may choose between a temporary
        table and view (defaulting to view).
+
        The append strategy can use a view because it will run a single INSERT statement.
-       When the unique_key is none,
-       then we can use a view because it will run a single INSERT statement.
+
+       When unique_key is none, the delete+insert strategy can use a view beacuse a
+       single INSERT statement is run with no DELETES as part of the statement.
        Otherwise, play it safe by using a temporary table.
   #} */
 
+  {% if language == "python" and tmp_relation_type is not none %}
+    {% do exceptions.raise_compiler_error(
+      "Python models currently only support 'table' for tmp_relation_type but "
+       ~ tmp_relation_type ~ " was specified."
+    ) %}
+  {% endif %}
+
+  {% if strategy == "delete+insert" and tmp_relation_type is not none and tmp_relation_type != "table" and unique_key is not none %}
+    {% do exceptions.raise_compiler_error(
+      "In order to maintain consistent results when `unique_key` is not none,
+      the `delete+insert` strategy only supports `table` for `tmp_relation_type` but "
+      ~ tmp_relation_type ~ " was specified."
+      )
+  %}
+  {% endif %}
+
   {% if language != "sql" %}
     {{ return("table") }}
-  {% elif strategy in ("default", "merge") and merge_tmp_relation_type == "table" %}
+  {% elif tmp_relation_type == "table" %}
     {{ return("table") }}
-  {% elif strategy in ("default", "merge") and merge_tmp_relation_type == "view" %}
+  {% elif tmp_relation_type == "view" %}
     {{ return("view") }}
   {% elif strategy in ("default", "merge", "append") %}
     {{ return("view") }}
