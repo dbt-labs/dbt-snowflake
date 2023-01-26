0fac983658907c6470e25a8b4475eb0525441aa1
Jeremy Cohen <jeremy@dbtlabs.com>
2023-01-26 15:18:27 +0100

    Convert caching_test (#395)
    * Convert caching_test
    
    * Switch dbt-core back to main

---
+++ tests/functional/adapter/test_caching.py
@@ -0,0 +1,14 @@
+import pytest
+from dbt.tests.adapter.caching.test_caching import BaseCachingLowercaseModel, BaseCachingUppercaseModel, BaseCachingSelectedSchemaOnly
+
+
+class TestCachingLowerCaseModel(BaseCachingLowercaseModel):
+    pass
+
+
+class TestCachingUppercaseModel(BaseCachingUppercaseModel):
+    pass
+
+
+class TestCachingSelectedSchemaOnly(BaseCachingSelectedSchemaOnly):
+    pass
