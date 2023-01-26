e7b7fd2ed0d2e33d08181fa57cadb0ff43d5d756
Matthew McKnight <91097623+McKnight-42@users.noreply.github.com>
2023-01-26 15:58:31 -0600

    [CT-1630] Convert column_types test for dbt-snowflake (#411)
    * init conversion of columns_types test for snowflake, removal of old test, dev_requirements pin changed
    
    * move large fixtures to own file to minimize test file
    
    * reset dev-requirement pointer

--- tests/integration/column_type_tests/macros/test_alter_column_type.sql
+++
@@ -1,5 +0,0 @@
--- Macro to alter a column type
-{% macro test_alter_column_type(model_name, column_name, new_column_type) %}
-  {% set relation = ref(model_name) %}
-  {{ alter_column_type(relation, column_name, new_column_type) }}
-{% endmacro %}
