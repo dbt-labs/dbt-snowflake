f9d80486563f1836f06af8c620468f32aadcb8ea
Matthew McKnight <91097623+McKnight-42@users.noreply.github.com>
2023-02-08 12:28:55 -0600

    remove graph_selection test (#453)
    * init push of graph-selection conversion
    
    * remove graph_selection test

--- tests/integration/graph_selection_tests/models/alternative.users.sql
+++
@@ -1,9 +0,0 @@
-{# Same as ´users´ model, but with dots in the model name #}
-{{
-    config(
-        materialized = 'table',
-        tags=['dots']
-    )
-}}
-
-select * from {{ ref('base_users') }}
