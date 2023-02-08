501bf963e98eaaa0f31bf3674baaf7c71939a823
Mila Page <versusfacit@users.noreply.github.com>
2023-02-08 13:50:37 -0800

    Remove unneeded import.

--- tests/functional/adapter/incremental/test_incremental_merge_exclude_columns.py
+++ tests/functional/adapter/incremental/test_incremental_merge_exclude_columns.py
@@ -1,6 +1,5 @@
 import pytest
 
-from dbt.tests.util import run_dbt
 from dbt.tests.adapter.incremental.test_incremental_merge_exclude_columns import BaseMergeExcludeColumns
 
 class TestMergeExcludeColumns(BaseMergeExcludeColumns):
