74d0585919f7387b856bdec6ae0284c7c54cf934
Emily Rockman <emily.rockman@dbtlabs.com>
2023-02-08 14:48:32 -0600

    first pass at fixing release test

--- setup.py
+++ setup.py
@@ -33,7 +33,8 @@ def _get_plugin_version_dict():
     _version_path = os.path.join(this_directory, "dbt", "adapters", "snowflake", "__version__.py")
     _semver = r"""(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)"""
     _pre = r"""((?P<prekind>a|b|rc)(?P<pre>\d+))?"""
-    _version_pattern = fr"""version\s*=\s*["']{_semver}{_pre}["']"""
+    _nightly = r"""(?P<nightly>\d+)?"""
+    _version_pattern = fr"""version\s*=\s*["']{_semver}{_pre}{_nightly}["']"""
     with open(_version_path) as f:
         match = re.search(_version_pattern, f.read().strip())
         if match is None:
