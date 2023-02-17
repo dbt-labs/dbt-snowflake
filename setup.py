d5fb8ac55aa9e116a92b6680cc22bd868628d450
Mike Alfare <mike.alfare@dbtlabs.com>
2023-02-17 12:11:23 -0500

    bumping snowflake-connector-python to latest 2.x

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "dbt-core~={}".format(dbt_core_version),
-        "snowflake-connector-python[secure-local-storage]>=2.4.1,<4.0.0",
+        "snowflake-connector-python[secure-local-storage]==2.9.0",
         "requests<3.0.0",
         "cryptography>=3.2,<40.0.0",
     ],
