34ef71e659247eec0685f910794ee0431af69dac
Michelle Ark <michelle.ark@dbtlabs.com>
2023-02-23 18:21:28 -0500

    fix test_constraints

--- tests/functional/adapter/test_constraints.py
+++ tests/functional/adapter/test_constraints.py
@@ -4,6 +4,12 @@ from dbt.tests.adapter.constraints.test_constraints import (
     BaseConstraintsColumnsEqual,
     BaseConstraintsRuntimeEnforcement
 )
+from dbt.tests.adapter.constraints.fixtures import (
+    my_model_wrong_order_sql,
+    my_model_wrong_name_sql,
+    model_schema_yml,
+    my_model_wrong_data_type_sql,
+)
 
 _expected_sql_snowflake = """
 create or replace transient table {0} (
@@ -18,8 +24,34 @@ create or replace transient table {0} (
 );
 """
 
+# Different on Snowflake:
+# - does not support a data type named 'int[]'
+constraints_yml = model_schema_yml.replace("int[]", "ARRAY")
+# - SELECT ARRAY[1,2,3]; is invalid. SELECT [1,2,3] works.
+my_model_wrong_data_type_sql = my_model_wrong_data_type_sql.replace("ARRAY", "")
+
+
 class TestSnowflakeConstraintsColumnsEqual(BaseConstraintsColumnsEqual):
-    pass
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "my_model_wrong_order.sql": my_model_wrong_order_sql,
+            "my_model_wrong_name.sql": my_model_wrong_name_sql,
+            "my_model_wrong_data_type.sql": my_model_wrong_data_type_sql,
+            "constraints_schema.yml": constraints_yml,
+        }
+
+    @pytest.fixture
+    def int_type(self):
+        return "FIXED"
+
+    @pytest.fixture
+    def int_array_type(self):
+        return "ARRAY"
+
+    @pytest.fixture
+    def string_array_type(self):
+        return "ARRAY"
 
 
 class TestSnowflakeConstraintsRuntimeEnforcement(BaseConstraintsRuntimeEnforcement):
