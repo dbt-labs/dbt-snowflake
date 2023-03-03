3a9fe8621f0ac237ea9b072d99ccb366ac645cf0
Michelle Ark <michelle.ark@dbtlabs.com>
2023-03-02 19:40:33 -0500

    test more types! + correct data_type schemas

--- tests/functional/adapter/test_constraints.py
+++ tests/functional/adapter/test_constraints.py
@@ -4,12 +4,6 @@ from dbt.tests.adapter.constraints.test_constraints import (
     BaseConstraintsColumnsEqual,
     BaseConstraintsRuntimeEnforcement
 )
-from dbt.tests.adapter.constraints.fixtures import (
-    my_model_wrong_order_sql,
-    my_model_wrong_name_sql,
-    model_schema_yml,
-    my_model_wrong_data_type_sql,
-)
 
 _expected_sql_snowflake = """
 create or replace transient table {0} (
@@ -24,34 +18,30 @@ create or replace transient table {0} (
 );
 """
 
-# Different on Snowflake:
-# - does not support a data type named 'int[]'
-constraints_yml = model_schema_yml.replace("int[]", "ARRAY")
-# - SELECT ARRAY[1,2,3]; is invalid. SELECT [1,2,3] works.
-my_model_wrong_data_type_sql = my_model_wrong_data_type_sql.replace("ARRAY", "")
-
 
 class TestSnowflakeConstraintsColumnsEqual(BaseConstraintsColumnsEqual):
-    @pytest.fixture(scope="class")
-    def models(self):
-        return {
-            "my_model_wrong_order.sql": my_model_wrong_order_sql,
-            "my_model_wrong_name.sql": my_model_wrong_name_sql,
-            "my_model_wrong_data_type.sql": my_model_wrong_data_type_sql,
-            "constraints_schema.yml": constraints_yml,
-        }
-
     @pytest.fixture
     def int_type(self):
         return "FIXED"
 
     @pytest.fixture
-    def int_array_type(self):
-        return "ARRAY"
+    def schema_int_type(self):
+        return "INT"
 
     @pytest.fixture
-    def string_array_type(self):
-        return "ARRAY"
+    def data_types(self, int_type, schema_int_type, string_type):
+        # sql_column_value, schema_data_type, error_data_type
+        return [
+            ['1', schema_int_type, int_type],
+            ["'1'", string_type, string_type],
+            ["cast('2019-01-01' as date)", 'date', 'DATE'],
+            ["true", 'boolean', 'BOOLEAN'],
+            ["'2013-11-03 00:00:00-07'::timestamptz", 'timestamp_tz', 'TIMESTAMP_TZ'],
+            ["'2013-11-03 00:00:00-07'::timestamp", 'timestamp', 'TIMESTAMP_NTZ'],
+            ["ARRAY_CONSTRUCT('a','b','c')", 'array', 'ARRAY'],
+            ["ARRAY_CONSTRUCT(1,2,3)", 'array', 'ARRAY'],
+            ["""TO_VARIANT(PARSE_JSON('{"key3": "value3", "key4": "value4"}'))""", 'variant', 'VARIANT'],
+        ]
 
 
 class TestSnowflakeConstraintsRuntimeEnforcement(BaseConstraintsRuntimeEnforcement):
