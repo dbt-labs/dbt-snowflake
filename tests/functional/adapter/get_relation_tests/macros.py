2e0376762d428d5942f7583d90a7ff4576a36235
Mike Alfare <mike.alfare@dbtlabs.com>
2023-03-10 19:59:19 -0500

    simplified test case, can't reproduce

--- tests/functional/adapter/get_relation_tests/macros.py
+++ tests/functional/adapter/get_relation_tests/macros.py
@@ -2,9 +2,9 @@
 MACRO_GET_RELATION = """
 {% macro get_relation(database, schema, identifier) %}
     {% set relation = adapter.get_relation(
-        database=database,
-        schema=schema,
-        identifier=identifier
+        database=target.database,
+        schema=target.schema,
+        identifier="FACT"
     ) %}
 
     {{ return(relation) }}
@@ -15,7 +15,7 @@ MACRO_GET_RELATION = """
 # mirrors dbt_utils._is_relation: https://github.com/dbt-labs/dbt-utils/blob/main/macros/jinja_helpers/_is_relation.sql
 # instead of throwing a compiler error, return the value of the check in the if statement
 MACRO_IS_RELATION = """
-{% macro is_relation(obj, macro) %}
+{% macro is_relation(obj) %}
     {% set if_condition = (obj is mapping and obj.get('metadata', {}).get('type', '').endswith('Relation')) %}
     {{ return(if_condition) }}
 {% endmacro %}
@@ -26,101 +26,17 @@ MACRO_IS_RELATION = """
 MACRO_CHECK_GET_RELATION_IS_RELATION = """
 {% macro check_get_relation_is_relation() %}
 
-    {% set my_relation = adapter.get_relation(
+    {% set relation = adapter.get_relation(
           database=target.database,
           schema=target.schema,
           identifier="FACT"
     ) %}
 
-    {% if not (my_relation is mapping and my_relation.get('metadata', {}).get('type', '').endswith('Relation')) %}
-        {% do exceptions.raise_compiler_error("Log: " ~ my_relation) %}
+    {% if not (relation is mapping and relation.get('metadata', {}).get('type', '').endswith('Relation')) %}
+        {% do exceptions.raise_compiler_error("Log: " ~ relation) %}
     {% endif %}
 
-    {{ return(my_relation) }}
+    {{ return(relation) }}
 
 {% endmacro %}
-"""
-
-
-# test whether the relation is found using `get_relation`
-# either way return all the pieces to the log by selecting them in the if block
-TEST_GET_RELATION_FACT = """
-{% set relation = get_relation(
-    database=target.database,
-    schema=target.schema,
-    identifier='FACT'
-) -%}
-
-{% if relation is none %}
-    select '{{ relation.path.database }}' as actual_database,
-           '{{ relation.path.schema }}' as actual_schema,
-           '{{ relation.path.identifier }}' as actual_table,
-           '{{ target.database }}' as expected_database,
-           '{{ target.schema }}' as expected_schema,
-           'fact' as expected_table,
-           {{ is_relation(relation) }} as is_relation_results
-{% else %}
-    select 1 where 1 = 0
-{% endif %}
-"""
-TEST_GET_RELATION_AD_HOC = """
-{% set relation = get_relation(
-    database=target.database,
-    schema=target.schema,
-    identifier='AD_HOC'
-) %}
-
-{%- if relation is none -%}
-    select '{{ relation.path.database }}' as actual_database,
-           '{{ relation.path.schema }}' as actual_schema,
-           '{{ relation.path.identifier }}' as actual_table,
-           '{{ target.database }}' as expected_database,
-           '{{ target.schema }}' as expected_schema,
-           'fact' as expected_table,
-           {{ is_relation(relation) }} as is_relation_results
-{% else %}
-    select 1 where 1 = 0
-{% endif %}
-"""
-
-
-# test whether the relation is found using `is_relation`
-# either way return all the pieces to the log by selecting them in the if block
-TEST_IS_RELATION_FACT = """
-{% set relation = adapter.Relation.create(
-    database=target.database,
-    schema=target.schema,
-    identifier='FACT'
-) %}
-
-{%- if not is_relation(relation) -%}
-    select '{{ relation.path.database }}' as actual_database,
-           '{{ relation.path.schema }}' as actual_schema,
-           '{{ relation.path.identifier }}' as actual_table,
-           '{{ target.database }}' as expected_database,
-           '{{ target.schema }}' as expected_schema,
-           'fact' as expected_table,
-           {{ is_relation(relation) }} as is_relation_results
-{% else %}
-    select 1 where 1 = 0
-{% endif %}
-"""
-TEST_IS_RELATION_AD_HOC = """
-{% set relation = adapter.Relation.create(
-    database=target.database,
-    schema=target.schema,
-    identifier='AD_HOC'
-) %}
-
-{%- if not is_relation(relation) -%}
-    select '{{ relation.path.database }}' as actual_database,
-           '{{ relation.path.schema }}' as actual_schema,
-           '{{ relation.path.identifier }}' as actual_table,
-           '{{ target.database }}' as expected_database,
-           '{{ target.schema }}' as expected_schema,
-           'fact' as expected_table,
-           {{ is_relation(relation) }} as is_relation_results
-{% else %}
-    select 1 where 1 = 0
-{% endif %}
 """
--- tests/functional/adapter/get_relation_tests/test_get_relation.py
+++ tests/functional/adapter/get_relation_tests/test_get_relation.py
@@ -1,23 +1,11 @@
-from typing import Mapping
-
 import pytest
 
 from dbt.tests.util import run_dbt
 
-from dbt.adapters.snowflake.relation import SnowflakeRelation
-
 from tests.functional.adapter.get_relation_tests import macros
 
 
-_MODEL_FACT = "select 1 as my_column"
-
-
-_MODEL_INVOKE_MACRO = """
-select '{{ check_get_relation_is_relation() }}' as this_should_return
-"""
-
-
-class GetRelationBase:
+class TestGetRelation:
 
     @pytest.fixture(scope="class")
     def project_config_update(self):
@@ -40,145 +28,28 @@ class GetRelationBase:
 
     @pytest.fixture(scope="class")
     def models(self):
-        """
-        dummy model to be checked for existence by `is_relation` and to be returned by `get_relation`
-        """
-        return {"FACT.sql": _MODEL_FACT}
-
-    @pytest.fixture(scope="class", autouse=True)
-    def setup_class(self, project):
-        run_dbt(["run"])
-
-    @pytest.fixture(scope="function")
-    def ad_hoc_table(self, project):
-        """
-        Manually create a table in the test schema, not using a model
-
-        This behaves differently from a table created via a model for `get_relation`
-        """
-        source_table = "FACT"
-        test_table = "AD_HOC"
-
-        create_sql = f"""
-            create or replace table {project.test_schema}.{test_table} as
-            select * from {project.test_schema}.{source_table}
-        """
-        verify_sql = f"select * from {project.test_schema}.{test_table}"
-        drop_sql = f"drop table if exists {project.test_schema}.{test_table}"
-
-        project.run_sql(create_sql)
-        project.run_sql(verify_sql)
-        yield
-        project.run_sql(drop_sql)
-
-
-class TestGetRelationPython(GetRelationBase):
-
-    @pytest.mark.parametrize("table_name,expected_result", [
-        ("FACT", True),
-        ("AD_HOC", True),
-    ])
-    def test_table_exists_with_standard_sql(self, project, ad_hoc_table, table_name, expected_result):
-        """
-        assert the table exists by trying to query it
-
-        This basically tests the setup and fixtures
-        """
-        verify_sql = f"select * from {project.test_schema}.{table_name}"
-        records = project.run_sql(verify_sql, fetch='one')
-        table_exists = (len(records) > 0)
-        assert table_exists == expected_result
-
-    @pytest.mark.parametrize("table_name,expected_result", [
-        ("FACT", True),         # this exists and should be returned
-        ("AD_HOC", False),  # this exists but should not be returned
-    ])
-    def test_table_exists_with_get_relation(self, project, ad_hoc_table, table_name, expected_result):
-        """
-        assert the table exists using `get_relation`
-
-        `get_relation` will return a relation if the relation exists as a model
-        `get_relation` will return None if the relation does not exist as a model, even if it exists as a table/view
-        """
-        get_relation = project.adapter.get_relation(
-            database=project.database,
-            schema=project.test_schema,
-            identifier=table_name
-        )
-        get_relation_returned_something = get_relation is not None
-        assert get_relation_returned_something is expected_result
-
-    @pytest.mark.parametrize("table_name,expected_result", [
-        ("FACT", True),
-        ("AD_HOC", True),
-    ])
-    def test_table_exists_with_is_relation(self, project, ad_hoc_table, table_name, expected_result):
-        """
-        assert the table exists using `is_relation`
-
-        `is_relation` will return True as long as `relation` is an instance of `BaseRelation`
-        """
-        relation = SnowflakeRelation.create(
-            database=project.database,
-            schema=project.test_schema,
-            identifier=table_name
-        )
-
-        is_relation = (
-            isinstance(relation, Mapping) and
-            relation.get("metadata", "").get("type", "").endswith("Relation")
-        )
-        assert is_relation == expected_result
-
-
-class TestGetRelationDBT(GetRelationBase):
+        return {"FACT.sql": "select 1 as my_column"}
 
     @pytest.fixture(scope="class")
     def macros(self):
         return {
             "get_relation.sql": macros.MACRO_GET_RELATION,
             "is_relation.sql": macros.MACRO_IS_RELATION,
+            "check_get_relation_is_relation.sql": macros.MACRO_CHECK_GET_RELATION_IS_RELATION,
         }
 
-    @pytest.fixture(scope="class")
-    def tests(self):
-        """
-        These tests mirror `test_table_exists_with_get_relation` and `test_table_exists_with_is_relation`
-        on `TestGetRelationPython` for `FACT` and `AD_HOC`
-        """
-        return {
-            "test_get_relation_fact.sql": macros.TEST_GET_RELATION_FACT,
-            "test_get_relation_ad_hoc.sql": macros.TEST_GET_RELATION_AD_HOC,
-            "test_is_relation_fact.sql": macros.TEST_IS_RELATION_FACT,
-            "test_is_relation_ad_hoc.sql": macros.TEST_IS_RELATION_AD_HOC,
-        }
+    @pytest.fixture(scope="class", autouse=True)
+    def setup_class(self, project):
+        run_dbt(["run"])
 
-    def test_get_relation_dbt(self, project, ad_hoc_table):
-        run_dbt(["test"])
+    def test_get_relation(self, project):
+        relation = project.adapter.execute_macro("get_relation")
+        assert relation.get("metadata", {}).get("type") == "SnowflakeRelation"
 
+    def test_is_relation(self, project):
+        relation = project.adapter.execute_macro("get_relation")
+        assert project.adapter.execute_macro("is_relation", kwargs={"obj": relation}) is True
 
-class TestGetRelationIsRelation(GetRelationBase):
-
-    @pytest.fixture(scope="class")
-    def models(self):
-        """
-        dummy model to be checked for existence by `is_relation` and to be returned by `get_relation`
-        model used to call the combination of `get_relation` and `is_relation`
-        """
-        return {
-            "FACT.sql": _MODEL_FACT,
-            "INVOKE_MACRO.sql": _MODEL_INVOKE_MACRO,
-        }
-
-    @pytest.fixture(scope="class")
-    def macros(self):
-        """
-        a macro that combines `get_relation` and `is_relation`
-        """
-        return {"check_get_relation_is_relation.sql": macros.MACRO_CHECK_GET_RELATION_IS_RELATION}
-
-    def test_it_runs(self, project):
-        """
-        The setup is the test
-        """
-        assert True
+    def test_check_get_relation_is_relation(self, project):
+        relation = project.adapter.execute_macro("check_get_relation_is_relation")
+        assert relation.get("metadata", {}).get("type") == "SnowflakeRelation"
