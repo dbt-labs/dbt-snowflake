6ca5acfac9ae6c8862f691c95b310fa705891bdd
Emily Rockman <emily.rockman@dbtlabs.com>
2023-03-27 09:38:26 -0500

    update to retrieve contract enforced from dict

--- dbt/include/snowflake/macros/adapters.sql
+++ dbt/include/snowflake/macros/adapters.sql
@@ -22,7 +22,8 @@
         {%- elif transient -%}
           transient
         {%- endif %} table {{ relation }}
-        {% if config.get('contract', False) %}
+        {%- set contract_config = config.get('contract', {}) -%}
+        {%- if contract_config.get('enforced', False) -%}
           {{ get_assert_columns_equivalent(sql) }}
           {{ get_columns_spec_ddl() }}
           {% set compiled_code = get_select_subquery(compiled_code) %}
@@ -95,7 +96,8 @@
     {{ get_persist_docs_column_list(model_columns, query_columns) }}
 
   {%- endif %}
-  {% if config.get('contract', False) -%}
+  {%- set contract_config = config.get('contract', {}) -%}
+  {%- if contract_config.get('enforced', False) -%}
     {{ get_assert_columns_equivalent(sql) }}
   {%- endif %}
   {% if copy_grants -%} copy grants {%- endif %} as (
