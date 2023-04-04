76c2aa47dad75f3ea9fc42926f0451738314b664
Mila Page <versusfacit@users.noreply.github.com>
2023-04-04 02:20:14 -0700

    Get rid of old test cases

--- tests/integration/simple_copy_test/models/get_and_ref.sql
+++
@@ -1,3 +0,0 @@
-{%- do adapter.get_relation(database=target.database, schema=target.schema, identifier='materialized') -%}
-
-select * from {{ ref('materialized') }}
--- tests/integration/simple_copy_test/tests/get_relation_quoting.sql
+++
@@ -1,9 +0,0 @@
-{%- set tgt = ref('seed') -%}
-{%- set got = adapter.get_relation(database=tgt.database, schema=tgt.schema, identifier=tgt.identifier) | string -%}
-{% set replaced = got.replace('"', '-') %}
-{% set expected = "-" + tgt.database.upper() + '-.-' + tgt.schema.upper() + '-.-' + tgt.identifier.upper() + '-' %}
-
-with cte as (
-    select '{{ replaced }}' as name
-)
-select * from cte where name not like '{{ expected }}'
