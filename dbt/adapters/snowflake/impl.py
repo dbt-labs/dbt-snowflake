f0fc80f8e0d2da6f908cb5a592a082e93d2913bb
Emily Rockman <emily.rockman@dbtlabs.com>
2023-04-11 13:51:16 -0500

    remove unused methods

--- dbt/adapters/snowflake/impl.py
+++ dbt/adapters/snowflake/impl.py
@@ -10,7 +10,6 @@ from dbt.adapters.sql.impl import (
     LIST_SCHEMAS_MACRO_NAME,
     LIST_RELATIONS_MACRO_NAME,
 )
-from dbt.contracts.graph.nodes import ConstraintType, ColumnLevelConstraint, ModelLevelConstraint
 
 from dbt.adapters.snowflake import SnowflakeConnectionManager
 from dbt.adapters.snowflake import SnowflakeRelation
@@ -255,17 +254,3 @@ CALL {proc_name}();
 
     def valid_incremental_strategies(self):
         return ["append", "merge", "delete+insert"]
-
-    @classmethod
-    def render_column_constraint(cls, constraint: ColumnLevelConstraint) -> str:
-        if constraint.type == ConstraintType.check:
-            return ""  # check not supported by snowflake
-        else:
-            return super().render_column_constraint(constraint)
-
-    @classmethod
-    def render_model_constraint(cls, constraint: ModelLevelConstraint) -> Optional[str]:
-        if constraint.type == ConstraintType.check:
-            return None  # check not supported by snowflake
-        else:
-            return super().render_model_constraint(constraint)
