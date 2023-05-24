a7f9ac1d338895e68afd61c24a8c4015cb0f2ff0
Mila Page <67295367+VersusFacit@users.noreply.github.com>
2023-05-24 12:09:11 -0700

    Backport #618 (#623)
    * Fix up test
    
    * Remove test that got merged from the future erroneously
    
    ---------
    
    Co-authored-by: Mila Page <versusfacit@users.noreply.github.com>

--- tests/functional/adapter/test_python_model.py
+++ tests/functional/adapter/test_python_model.py
@@ -1,16 +1,10 @@
 import pytest
-from dbt.tests.util import run_dbt, write_file, run_sql_with_adapter
-from dbt.tests.adapter.python_model.test_python_model import BasePythonModelTests, BasePythonIncrementalTests
 
-class TestPythonModelSnowflake(BasePythonModelTests):
-    @pytest.fixture(scope="class")
-    def project_config_update(self):
-        return {
-            "models":{ "use_anonymous_sproc": True}
-        }
-
-class TestIncrementalSnowflake(BasePythonIncrementalTests):
-    pass
+from dbt.tests.util import run_dbt, write_file
+from dbt.tests.adapter.python_model.test_python_model import (
+    BasePythonModelTests,
+    BasePythonIncrementalTests,
+)
 
 models__simple_python_model = """
 import pandas
@@ -22,6 +16,7 @@ def model(dbt, session):
     data = [[1,2]] * 10
     return pandas.DataFrame(data, columns=['test', 'test2'])
 """
+
 models__simple_python_model_v2 = """
 import pandas
 
@@ -33,6 +28,35 @@ def model(dbt, session):
     return pandas.DataFrame(data, columns=['test1', 'test3'])
 """
 
+models__custom_target_model = """
+import pandas
+
+def model(dbt, session):
+    dbt.config(
+        materialized="table",
+        schema="MY_CUSTOM_SCHEMA",
+        alias="_TEST_PYTHON_MODEL",
+    )
+
+    df = pandas.DataFrame({
+        'City': ['Buenos Aires', 'Brasilia', 'Santiago', 'Bogota', 'Caracas'],
+        'Country': ['Argentina', 'Brazil', 'Chile', 'Colombia', 'Venezuela'],
+        'Latitude': [-34.58, -15.78, -33.45, 4.60, 10.48],
+        'Longitude': [-58.66, -47.91, -70.66, -74.08, -66.86]
+    })
+
+    return df
+"""
+
+class TestPythonModelSnowflake(BasePythonModelTests):
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {
+            "models":{ "use_anonymous_sproc": True}
+        }
+
+class TestIncrementalSnowflake(BasePythonIncrementalTests):
+    pass
 
 
 class TestChangingSchemaSnowflake:
@@ -51,7 +75,7 @@ import sys
 from snowflake.snowpark.types import StructType, FloatType, StringType, StructField
 
 def model( dbt, session):
-    
+
     dbt.config(
         materialized='table',
         imports = ['@dbt_integration_test/iris.csv'],
@@ -83,4 +107,16 @@ class TestImportSnowflake:
         project.run_sql("create or replace STAGE dbt_integration_test")
         project.run_sql(f"PUT file://{project.project_root}/seeds/iris.csv @dbt_integration_test/;")
         run_dbt(["run"])
-    
+
+
+# https://github.com/dbt-labs/dbt-snowflake/issues/393 is notorious for being triggered on some
+# environments but not others. As of writing this, we don't know the true root cause. This test may
+# not fail on all systems with problems regarding custom schema model configurations.
+class TestCustomSchemaWorks:
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {"custom_target_model.py": models__custom_target_model}
+
+    def test_custom_target(self, project):
+        results = run_dbt()
+        assert results[0].node.schema == f"{project.test_schema}_MY_CUSTOM_SCHEMA"
