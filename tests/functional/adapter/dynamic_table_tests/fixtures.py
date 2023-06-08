61ff6c4c4e8d7b877cc222686064c10135af39c0
Mike Alfare <mike.alfare@dbtlabs.com>
2023-06-07 20:17:04 -0400

    updated with materialized views pushed to main, added retry to get_row_count for dynamic table initialization

--- tests/functional/adapter/dynamic_table_tests/fixtures.py
+++ tests/functional/adapter/dynamic_table_tests/fixtures.py
@@ -1,10 +1,39 @@
 from time import sleep
+from datetime import datetime
 
 import pytest
+from snowflake.connector.errors import ProgrammingError
 
-from dbt.tests.util import relation_from_name, run_sql_with_adapter
+from dbt.dataclass_schema import StrEnum
+from dbt.tests.util import relation_from_name, run_sql_with_adapter, get_manifest
 from dbt.tests.adapter.materialized_view.base import Base
 from dbt.tests.adapter.materialized_view.on_configuration_change import OnConfigurationChangeBase
+
+
+def refresh_dynamic_table(adapter, model: str):
+    sql = f"alter dynamic table {model} set lag = '60 seconds'"
+    run_sql_with_adapter(adapter, sql)
+
+
+def get_row_count(project, model: str) -> int:
+    sql = f"select count(*) from {project.database}.{project.test_schema}.{model};"
+
+    now = datetime.now()
+    while (datetime.now() - now).total_seconds() < 120:
+        try:
+            return project.run_sql(sql, fetch="one")[0]
+        except ProgrammingError:
+            sleep(5)
+    raise ProgrammingError("90 seconds has passed and the dynamic table is still not initialized.")
+
+
+def assert_model_exists_and_is_correct_type(project, model: str, relation_type: StrEnum):
+    # In general, `relation_type` will be of type `RelationType`.
+    # However, in some cases (e.g. `dbt-snowflake`) adapters will have their own `RelationType`.
+    manifest = get_manifest(project.project_root)
+    model_metadata = manifest.nodes[f"model.test.{model}"]
+    assert model_metadata.config.materialized == relation_type
+    assert get_row_count(project, model) >= 0
 
 
 class SnowflakeBasicBase(Base):
@@ -23,11 +52,6 @@ class SnowflakeBasicBase(Base):
         select * from {{ ref('base_table') }}
         """
         return {"base_table.sql": base_table, "base_dynamic_table.sql": base_dynamic_table}
-
-    def refresh_dynamic_table(self, adapter):
-        sql = "alter dynamic table base_dynamic_table set lag = '60 seconds'"
-        run_sql_with_adapter(adapter, sql)
-        sleep(90)
 
 
 class SnowflakeOnConfigurationChangeBase(OnConfigurationChangeBase):
--- tests/functional/adapter/dynamic_table_tests/test_dynamic_table.py
+++ tests/functional/adapter/dynamic_table_tests/test_dynamic_table.py
@@ -1,24 +1,26 @@
 import pytest
 
 from dbt.contracts.results import RunStatus
-from dbt.adapters.snowflake.relation import SnowflakeRelationType
+from dbt.contracts.graph.model_config import OnConfigurationChangeOption
 from dbt.tests.adapter.materialized_view.base import (
     run_model,
-    assert_model_exists_and_is_correct_type,
     insert_record,
-    get_row_count,
 )
 from dbt.tests.adapter.materialized_view.on_configuration_change import assert_proper_scenario
 
+from dbt.adapters.snowflake.relation import SnowflakeRelationType
 from tests.functional.adapter.dynamic_table_tests.fixtures import (
     SnowflakeBasicBase,
     SnowflakeOnConfigurationChangeBase,
+    assert_model_exists_and_is_correct_type,
+    refresh_dynamic_table,
+    get_row_count,
 )
 
 
 class TestBasic(SnowflakeBasicBase):
     def test_relation_is_dynamic_table_on_initial_creation(self, project, adapter):
-        self.refresh_dynamic_table(adapter)
+        refresh_dynamic_table(adapter, "base_dynamic_table")
         assert_model_exists_and_is_correct_type(
             project, "base_dynamic_table", SnowflakeRelationType.DynamicTable
         )
@@ -26,21 +28,21 @@ class TestBasic(SnowflakeBasicBase):
 
     def test_relation_is_dynamic_table_when_rerun(self, project, adapter):
         run_model("base_dynamic_table")
-        self.refresh_dynamic_table(adapter)
+        refresh_dynamic_table(adapter, "base_dynamic_table")
         assert_model_exists_and_is_correct_type(
             project, "base_dynamic_table", SnowflakeRelationType.DynamicTable
         )
 
     def test_relation_is_dynamic_table_on_full_refresh(self, project, adapter):
         run_model("base_dynamic_table", full_refresh=True)
-        self.refresh_dynamic_table(adapter)
+        refresh_dynamic_table(adapter, "base_dynamic_table")
         assert_model_exists_and_is_correct_type(
             project, "base_dynamic_table", SnowflakeRelationType.DynamicTable
         )
 
     def test_relation_is_dynamic_table_on_update(self, project, adapter):
         run_model("base_dynamic_table", run_args=["--vars", "quoting: {identifier: True}"])
-        self.refresh_dynamic_table(adapter)
+        refresh_dynamic_table(adapter, "base_dynamic_table")
         assert_model_exists_and_is_correct_type(
             project, "base_dynamic_table", SnowflakeRelationType.DynamicTable
         )
@@ -48,7 +50,7 @@ class TestBasic(SnowflakeBasicBase):
     def test_updated_base_table_data_only_shows_in_dynamic_table_after_rerun(
         self, project, adapter
     ):
-        self.refresh_dynamic_table(adapter)
+        refresh_dynamic_table(adapter, "base_dynamic_table")
 
         # poll database
         table_start = get_row_count(project, "base_table")
@@ -64,7 +66,6 @@ class TestBasic(SnowflakeBasicBase):
 
         # refresh the dynamic table
         run_model("base_dynamic_table")
-        self.refresh_dynamic_table(adapter)
 
         # poll database
         table_end = get_row_count(project, "base_table")
@@ -75,14 +76,18 @@ class TestBasic(SnowflakeBasicBase):
         assert dyn_start == dyn_mid < dyn_end
 
 
-class OnConfigurationChangeCommon(SnowflakeOnConfigurationChangeBase):
+@pytest.mark.skip("We're not looking for changes yet")
+class TestOnConfigurationChangeApply(SnowflakeOnConfigurationChangeBase):
+    # we don't need to specify OnConfigurationChangeOption.Apply because it's the default
+    # this is part of the test
+
     def test_full_refresh_takes_precedence_over_any_configuration_changes(
         self, configuration_changes, replace_message, configuration_change_message, adapter
     ):
-        self.refresh_dynamic_table(adapter)
+        refresh_dynamic_table(adapter, "base_dynamic_table")
         results, logs = run_model("base_dynamic_table", full_refresh=True)
         assert_proper_scenario(
-            self.on_configuration_change,
+            OnConfigurationChangeOption.Apply,
             results,
             logs,
             RunStatus.Success,
@@ -93,25 +98,23 @@ class OnConfigurationChangeCommon(SnowflakeOnConfigurationChangeBase):
     def test_model_is_refreshed_with_no_configuration_changes(
         self, refresh_message, configuration_change_message, adapter
     ):
-        self.refresh_dynamic_table(adapter)
+        refresh_dynamic_table(adapter, "base_dynamic_table")
         results, logs = run_model("base_dynamic_table")
         assert_proper_scenario(
-            self.on_configuration_change,
+            OnConfigurationChangeOption.Apply,
             results,
             logs,
             RunStatus.Success,
             messages_in_logs=[refresh_message, configuration_change_message],
         )
 
-
-class TestOnConfigurationChangeApply(OnConfigurationChangeCommon):
-    @pytest.mark.skip("This fails because there are no changes in the stub")
     def test_model_applies_changes_with_configuration_changes(
-        self, configuration_changes, alter_message, update_index_message
+        self, configuration_changes, alter_message, update_index_message, adapter
     ):
+        refresh_dynamic_table(adapter, "base_dynamic_table")
         results, logs = run_model("base_dynamic_table")
         assert_proper_scenario(
-            self.on_configuration_change,
+            OnConfigurationChangeOption.Apply,
             results,
             logs,
             RunStatus.Success,
@@ -119,16 +122,46 @@ class TestOnConfigurationChangeApply(OnConfigurationChangeCommon):
         )
 
 
-class TestOnConfigurationChangeContinue(OnConfigurationChangeCommon):
-    on_configuration_change = "continue"
+@pytest.mark.skip("We're not looking for changes yet")
+class TestOnConfigurationChangeContinue(SnowflakeOnConfigurationChangeBase):
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {"models": {"on_configuration_change": OnConfigurationChangeOption.Continue.value}}
 
-    @pytest.mark.skip("This fails because there are no changes in the stub")
-    def test_model_is_skipped_with_configuration_changes(
-        self, configuration_changes, configuration_change_skip_message
+    def test_full_refresh_takes_precedence_over_any_configuration_changes(
+        self, configuration_changes, replace_message, configuration_change_message, adapter
     ):
+        refresh_dynamic_table(adapter, "base_dynamic_table")
+        results, logs = run_model("base_dynamic_table", full_refresh=True)
+        assert_proper_scenario(
+            OnConfigurationChangeOption.Continue,
+            results,
+            logs,
+            RunStatus.Success,
+            messages_in_logs=[replace_message],
+            messages_not_in_logs=[configuration_change_message],
+        )
+
+    def test_model_is_refreshed_with_no_configuration_changes(
+        self, refresh_message, configuration_change_message, adapter
+    ):
+        refresh_dynamic_table(adapter, "base_dynamic_table")
         results, logs = run_model("base_dynamic_table")
         assert_proper_scenario(
-            self.on_configuration_change,
+            OnConfigurationChangeOption.Continue,
+            results,
+            logs,
+            RunStatus.Success,
+            messages_in_logs=[refresh_message, configuration_change_message],
+        )
+
+    def test_model_is_skipped_with_configuration_changes(
+        self, configuration_changes, configuration_change_skip_message, adapter
+    ):
+        refresh_dynamic_table(adapter, "base_dynamic_table")
+        results, logs = run_model("base_dynamic_table")
+        assert_proper_scenario(
+            OnConfigurationChangeOption.Continue,
             results,
             logs,
             RunStatus.Success,
@@ -136,16 +169,46 @@ class TestOnConfigurationChangeContinue(OnConfigurationChangeCommon):
         )
 
 
-class TestOnConfigurationChangeFail(OnConfigurationChangeCommon):
-    on_configuration_change = "fail"
+@pytest.mark.skip("We're not looking for changes yet")
+class TestOnConfigurationChangeFail(SnowflakeOnConfigurationChangeBase):
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {"models": {"on_configuration_change": OnConfigurationChangeOption.Fail.value}}
 
-    @pytest.mark.skip("This fails because there are no changes in the stub")
-    def test_run_fails_with_configuration_changes(
-        self, configuration_changes, configuration_change_fail_message
+    def test_full_refresh_takes_precedence_over_any_configuration_changes(
+        self, configuration_changes, replace_message, configuration_change_message, adapter
     ):
+        refresh_dynamic_table(adapter, "base_dynamic_table")
+        results, logs = run_model("base_dynamic_table", full_refresh=True)
+        assert_proper_scenario(
+            OnConfigurationChangeOption.Fail,
+            results,
+            logs,
+            RunStatus.Success,
+            messages_in_logs=[replace_message],
+            messages_not_in_logs=[configuration_change_message],
+        )
+
+    def test_model_is_refreshed_with_no_configuration_changes(
+        self, refresh_message, configuration_change_message, adapter
+    ):
+        refresh_dynamic_table(adapter, "base_dynamic_table")
+        results, logs = run_model("base_dynamic_table")
+        assert_proper_scenario(
+            OnConfigurationChangeOption.Fail,
+            results,
+            logs,
+            RunStatus.Success,
+            messages_in_logs=[refresh_message, configuration_change_message],
+        )
+
+    def test_run_fails_with_configuration_changes(
+        self, configuration_changes, configuration_change_fail_message, adapter
+    ):
+        refresh_dynamic_table(adapter, "base_dynamic_table")
         results, logs = run_model("base_dynamic_table", expect_pass=False)
         assert_proper_scenario(
-            self.on_configuration_change,
+            OnConfigurationChangeOption.Fail,
             results,
             logs,
             RunStatus.Error,
