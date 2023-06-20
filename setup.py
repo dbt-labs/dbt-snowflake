fa1343e98db89e8fd62268e2458d07082737681f
Colin <colin.rogers@dbtlabs.com>
2023-06-20 13:07:51 -0700

    remove dup classifiers entry

--- setup.py
+++ setup.py
@@ -78,7 +78,6 @@ setup(
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
