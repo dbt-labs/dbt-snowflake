13edf7c45b919751b232ed4b10b332017c910359
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2023-06-21 16:29:56 -0500

    change state_relation to defer_relation to match change in dbt-core

--- dbt/include/snowflake/macros/materializations/clone.sql
+++ dbt/include/snowflake/macros/materializations/clone.sql
@@ -2,10 +2,10 @@
     {{ return(True) }}
 {% endmacro %}
 
-{% macro snowflake__get_clone_table_sql(this_relation, state_relation) %}
+{% macro snowflake__create_or_replace_clone(this_relation, defer_relation) %}
     create or replace
       {{ "transient" if config.get("transient", true) }}
       table {{ this_relation }}
-      clone {{ state_relation }}
+      clone {{ defer_relation }}
       {{ "copy grants" if config.get("copy_grants", false) }}
 {% endmacro %}
