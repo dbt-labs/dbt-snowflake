2414226224fcbbfe4fcda46a73eb6d2ea47b52fb
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2023-06-26 17:07:10 -0500

    add clean_up method to drop alt schema names after tests run

--- tests/functional/adapter/dbt_clone/test_dbt_clone.py
+++ tests/functional/adapter/dbt_clone/test_dbt_clone.py
@@ -1,5 +1,20 @@
+import pytest
 from dbt.tests.adapter.dbt_clone.test_dbt_clone import BaseClonePossible
 
 
 class TestSnowflakeClonePossible(BaseClonePossible):
+    @pytest.fixture(autouse=True)
+    def clean_up(self, project):
+        yield
+        with project.adapter.connection_named("__test"):
+            relation = project.adapter.Relation.create(
+                database=project.database, schema=f"{project.test_schema}_SEEDS"
+            )
+            project.adapter.drop_schema(relation)
+
+            relation = project.adapter.Relation.create(
+                database=project.database, schema=project.test_schema
+            )
+            project.adapter.drop_schema(relation)
+
     pass
