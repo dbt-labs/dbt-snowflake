5b3f8bfb5f09246fb1c3c578a3628f1360e8b11e
Mike Alfare <13974384+mikealfare@users.noreply.github.com>
2023-07-06 19:43:28 -0400

    implement new refresh logic, remove flaky workaround (#684)

--- tests/functional/adapter/dynamic_table_tests/fixtures.py
+++ tests/functional/adapter/dynamic_table_tests/fixtures.py
@@ -1,11 +1,6 @@
-from time import sleep
-from datetime import datetime
-
 import pytest
-from snowflake.connector.errors import ProgrammingError
 
-from dbt.dataclass_schema import StrEnum
-from dbt.tests.util import relation_from_name, get_manifest
+from dbt.tests.util import relation_from_name
 from dbt.tests.adapter.materialized_view.base import Base
 from dbt.tests.adapter.materialized_view.on_configuration_change import OnConfigurationChangeBase
 
@@ -33,34 +28,6 @@ BASE_DYNAMIC_TABLE_UPDATED = """
 ) }}
 select * from {{ ref('base_table') }}
 """
-
-
-def refresh_dynamic_table(adapter, model: str):
-    # there's no forced refresh, so just wait
-    sleep(80)
-
-
-def get_row_count(project, model: str) -> int:
-    sql = f"select count(*) from {project.database}.{project.test_schema}.{model};"
-
-    now = datetime.now()
-    while (datetime.now() - now).total_seconds() < 2 * TARGET_LAG_IN_S:
-        try:
-            return project.run_sql(sql, fetch="one")[0]
-        except ProgrammingError:
-            sleep(5)
-    raise ProgrammingError(
-        f"{2 * TARGET_LAG_IN_S} seconds has passed and the dynamic table is still not initialized."
-    )
-
-
-def assert_model_exists_and_is_correct_type(project, model: str, relation_type: StrEnum):
-    # In general, `relation_type` will be of type `RelationType`.
-    # However, in some cases (e.g. `dbt-snowflake`) adapters will have their own `RelationType`.
-    manifest = get_manifest(project.project_root)
-    model_metadata = manifest.nodes[f"model.test.{model}"]
-    assert model_metadata.config.materialized == relation_type
-    assert get_row_count(project, model) >= 0
 
 
 class SnowflakeBasicBase(Base):
