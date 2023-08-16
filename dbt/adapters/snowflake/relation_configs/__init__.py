a465707e5be412673522a7621ce3a4a4c087c5fb
Matthew McKnight <91097623+McKnight-42@users.noreply.github.com>
2023-08-16 14:35:38 -0500

    manual backport of pr 732 (#747)

--- dbt/adapters/snowflake/relation_configs/__init__.py
+++ dbt/adapters/snowflake/relation_configs/__init__.py
@@ -2,14 +2,10 @@ from dbt.adapters.snowflake.relation_configs.dynamic_table import (
     SnowflakeDynamicTableConfig,
     SnowflakeDynamicTableConfigChangeset,
     SnowflakeDynamicTableWarehouseConfigChange,
+    SnowflakeDynamicTableTargetLagConfigChange,
 )
 from dbt.adapters.snowflake.relation_configs.policies import (
     SnowflakeIncludePolicy,
     SnowflakeQuotePolicy,
     SnowflakeRelationType,
-)
-from dbt.adapters.snowflake.relation_configs.target_lag import (
-    SnowflakeDynamicTableTargetLagConfig,
-    SnowflakeDynamicTableTargetLagConfigChange,
-    SnowflakeDynamicTableTargetLagPeriod,
 )
