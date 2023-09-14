dfd5940dd4c6c9d4c1a3fffd2c29c20aa5764431
Josh Devlin <joshdevlin@canva.com>
2023-09-14 14:22:42 +1000

    Add failing test

---
+++ tests/functional/duplicate_key_unlogged/test_duplicate_key_not_in_exceptions.py
@@ -0,0 +1,32 @@
+import pytest
+
+from dbt.tests.util import (
+    run_dbt,
+)
+
+_MODELS__view = """
+{{ config(
+    materialized='view',
+) }}
+
+with dupes as (
+    select 'foo' as key, 1 as value
+    union all
+    select 'foo' as key, 2 as value
+)
+
+select
+    object_agg(key, value)
+from dupes
+"""
+
+
+class TestRowValuesNotInExceptions:
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {"model.sql": _MODELS__incremental_model}
+
+    def test_row_values_were_scrubbed_from_duplicate_merge_exception(self, project):
+        result = run_dbt(["run", "-s", "model"])
+        assert len(result) == 1
+        assert "Duplicate field key [redacted]" in result[0].message
