669e6dfdfb5f6cb7d358f7feb6bf5f57c97841da
Michelle Ark <michelle.ark@dbtlabs.com>
2023-09-28 19:10:26 +0100

    reuse core fixture for dbt show sql header test

--- tests/functional/adapter/dbt_show/test_dbt_show.py
+++ tests/functional/adapter/dbt_show/test_dbt_show.py
@@ -1,18 +1,4 @@
-import pytest
 from dbt.tests.adapter.dbt_show.test_dbt_show import BaseShowSqlHeader, BaseShowLimit
-
-
-my_model_sql_header_sql = """
-{{
-  config(
-    materialized = "table"
-  )
-}}
-{% call set_sql_header(config) %}
-SET MY_VARIABLE='test';
-{% endcall %}
-SELECT $MY_VARIABLE as column_name
-"""
 
 
 class TestBigQueryShowLimit(BaseShowLimit):
@@ -20,8 +6,4 @@ class TestBigQueryShowLimit(BaseShowLimit):
 
 
 class TestBigQueryShowSqlHeader(BaseShowSqlHeader):
-    @pytest.fixture(scope="class")
-    def models(self):
-        return {
-            "sql_header.sql": my_model_sql_header_sql,
-        }
+    pass
