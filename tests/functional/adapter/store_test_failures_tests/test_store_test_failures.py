7a5875b9b0e29a810adbb8c8ff93075d2f6ea2ca
Mike Alfare <mike.alfare@dbtlabs.com>
2023-09-28 22:34:43 -0400

    update changelog and test names to reflect renamed parameter

--- tests/functional/adapter/store_test_failures_tests/test_store_test_failures.py
+++ tests/functional/adapter/store_test_failures_tests/test_store_test_failures.py
@@ -1,7 +1,12 @@
+from dbt.tests.adapter.store_test_failures_tests.basic import StoreTestFailures
 from dbt.tests.adapter.store_test_failures_tests.test_store_test_failures import (
     TestStoreTestFailures,
 )
 
 
 class SnowflakeTestStoreTestFailures(TestStoreTestFailures):
+    pass
+
+
+class TestStoreTestFailures(StoreTestFailures):
     pass
--- tests/functional/adapter/test_persist_test_results.py
+++
@@ -1,5 +0,0 @@
-from dbt.tests.adapter.persist_test_results.basic import PersistTestResults
-
-
-class TestPersistTestResults(PersistTestResults):
-    pass
