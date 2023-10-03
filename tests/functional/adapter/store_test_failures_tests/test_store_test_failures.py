e02644234e94ee96e973888c3f14d764bac994f8
Mike Alfare <mike.alfare@dbtlabs.com>
2023-10-03 16:22:21 -0400

    implement store-failures-as test cases

--- tests/functional/adapter/store_test_failures_tests/test_store_test_failures.py
+++ tests/functional/adapter/store_test_failures_tests/test_store_test_failures.py
@@ -1,12 +1,29 @@
-from dbt.tests.adapter.store_test_failures_tests.basic import StoreTestFailures
+from dbt.tests.adapter.store_test_failures_tests.basic import (
+    StoreTestFailuresAsGeneric,
+    StoreTestFailuresAsInteractions,
+    StoreTestFailuresAsProjectLevelOff,
+    StoreTestFailuresAsProjectLevelView,
+)
 from dbt.tests.adapter.store_test_failures_tests.test_store_test_failures import (
     TestStoreTestFailures,
 )
 
 
-class SnowflakeTestStoreTestFailures(TestStoreTestFailures):
+class TestSnowflakeStoreTestFailures(TestStoreTestFailures):
     pass
 
 
-class TestStoreTestFailures(StoreTestFailures):
+class TestSnowflakeStoreTestFailuresAsInteractions(StoreTestFailuresAsInteractions):
+    pass
+
+
+class TestSnowflakeStoreTestFailuresAsProjectLevelOff(StoreTestFailuresAsProjectLevelOff):
+    pass
+
+
+class TestSnowflakeStoreTestFailuresAsProjectLevelView(StoreTestFailuresAsProjectLevelView):
+    pass
+
+
+class TestSnowflakeStoreTestFailuresAsGeneric(StoreTestFailuresAsGeneric):
     pass
