63a61f3e7375c915c7ab921869bdcbdaa7528f87
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2024-01-18 14:35:11 -0600

    try and pin common and adapters

--- setup.py
+++ setup.py
@@ -68,8 +68,8 @@ setup(
     include_package_data=True,
     install_requires=[
         "dbt-core~={}".format(dbt_core_version),
-        "dbt-common<1.0",
-        "dbt-adapters @ git+https://github.com/dbt-labs/dbt-adapters.git",
+        "dbt-common~=0.1.0",
+        "dbt-adapters~=0.1.0",
         "snowflake-connector-python[secure-local-storage]~=3.0",
         # installed via dbt-core but referenced directly; don't pin to avoid version conflicts with dbt-core
         "agate",
