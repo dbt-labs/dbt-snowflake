694c515d433edef51974715ed92077fc568a379d
Michelle Ark <michelle.ark@dbtlabs.com>
2024-02-01 13:11:47 -0500

    array and json support

--- tests/functional/adapter/unit_testing/test_unit_testing.py
+++ tests/functional/adapter/unit_testing/test_unit_testing.py
@@ -17,11 +17,10 @@ class TestSnowflakeUnitTestingTypes(BaseUnitTestingTypes):
             ["'2013-11-03 00:00:00-0'::TIMESTAMPTZ", "2013-11-03 00:00:00-0"],
             ["TO_NUMBER('3', 10, 9)", "3"],
             # ["3::VARIANT", "3"],
-            # [
-            #     """TO_JSON(PARSE_JSON('{"bar": "baz", "balance": 7.77, "active": false}'))""",
-            #     """'TO_JSON(PARSE_JSON('{"bar": "baz", "balance": 7.77, "active": false}'))'""",
-            # ],
-            # TODO: support complex types
-            # ["ARRAY['a','b','c']", """'{"a", "b", "c"}'"""],
-            # ["ARRAY[1,2,3]", """'{1, 2, 3}'"""],
+            [
+                "{'Alberta':'Edmonton','Manitoba':'Winnipeg'}",
+                "{'Alberta':'Edmonton','Manitoba':'Winnipeg'}",
+            ],
+            ["['a','b','c']", "['a','b','c']"],
+            ["[1,2,3]", "[1, 2, 3]"],
         ]
