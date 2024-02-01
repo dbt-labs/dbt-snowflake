a2e1987184f3c8c22cfcbc7dfeb04607cf01bbdf
Michelle Ark <michelle.ark@dbtlabs.com>
2024-02-01 13:22:55 -0500

    variant support

--- dbt/include/snowflake/macros/utils/safe_cast.sql
+++ dbt/include/snowflake/macros/utils/safe_cast.sql
@@ -1,4 +1,8 @@
 {% macro snowflake__safe_cast(field, type) %}
-    {% set field_as_sting =  dbt.string_literal(field) if field is number else field %}
-    try_cast({{field_as_sting}} as {{type}})
+    {% if (type|upper != "VARIANT") -%}
+        {% set field_as_sting =  dbt.string_literal(field) if field is number else field %}
+        try_cast({{field_as_sting}} as {{type}})
+    {% else -%}
+        cast({{field}} as {{type}})
+    {% endif -%}
 {% endmacro %}
--- tests/functional/adapter/unit_testing/test_unit_testing.py
+++ tests/functional/adapter/unit_testing/test_unit_testing.py
@@ -9,6 +9,7 @@ class TestSnowflakeUnitTestingTypes(BaseUnitTestingTypes):
         # sql_value, yaml_value
         return [
             ["1", "1"],
+            ["2.0", "2.0"],
             ["'12345'", "12345"],
             ["'string'", "string"],
             ["true", "true"],
@@ -16,7 +17,7 @@ class TestSnowflakeUnitTestingTypes(BaseUnitTestingTypes):
             ["TIMESTAMP '2013-11-03 00:00:00-0'", "2013-11-03 00:00:00-0"],
             ["'2013-11-03 00:00:00-0'::TIMESTAMPTZ", "2013-11-03 00:00:00-0"],
             ["TO_NUMBER('3', 10, 9)", "3"],
-            # ["3::VARIANT", "3"],
+            ["3::VARIANT", "3"],
             [
                 "{'Alberta':'Edmonton','Manitoba':'Winnipeg'}",
                 "{'Alberta':'Edmonton','Manitoba':'Winnipeg'}",
