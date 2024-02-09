d01d63cfa1df170d089e7caeebe14f361b4e78b3
Michelle Ark <MichelleArk@users.noreply.github.com>
2024-02-09 11:49:59 -0500

    [Snowflake] test types for unit testing (#896)
    * first pass: test unit testing on snowflake
    
    * array and json support
    
    * variant support
    
    * refactor to implement cast
    
    * geography and geometry types + cast refactor
    
    * fix variant logic in safe_cast
    
    * typo
    
    * TestSnowflakeUnitTestCaseInsensitivity
    
    * changelog entry
    
    * TestSnowflakeUnitTestInvalidInput
    
    * restore dev-requirements
    
    ---------
    
    Co-authored-by: Mike Alfare <13974384+mikealfare@users.noreply.github.com>

--- dbt/include/snowflake/macros/utils/safe_cast.sql
+++ dbt/include/snowflake/macros/utils/safe_cast.sql
@@ -1,3 +1,13 @@
 {% macro snowflake__safe_cast(field, type) %}
-    try_cast({{field}} as {{type}})
+    {% if type|upper == "GEOMETRY" -%}
+        try_to_geometry({{field}})
+    {% elif type|upper == "GEOGRAPHY" -%}
+        try_to_geography({{field}})
+    {% elif type|upper != "VARIANT" -%}
+        {#-- Snowflake try_cast does not support casting to variant, and expects the field as a string --#}
+        {% set field_as_string =  dbt.string_literal(field) if field is number else field %}
+        try_cast({{field_as_string}} as {{type}})
+    {% else -%}
+        {{ adapter.dispatch('cast', 'dbt')(field, type) }}
+    {% endif -%}
 {% endmacro %}
