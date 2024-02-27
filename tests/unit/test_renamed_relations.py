5507afb2b655283d40d8f17e719eb0623f7d93f0
Mila Page <versusfacit@users.noreply.github.com>
2024-02-27 02:21:27 -0800

    Fix test.

--- tests/unit/test_renamed_relations.py
+++ tests/unit/test_renamed_relations.py
@@ -1,16 +1,17 @@
-from dbt.adapters.bigquery.relation import BigQueryRelation
-from dbt.adapters.contracts.relation import RelationType
+from dbt.adapters.snowflake.relation import SnowflakeRelation
+from dbt.adapters.snowflake.relation_configs import SnowflakeRelationType
 
 
 def test_renameable_relation():
-    relation = BigQueryRelation.create(
+    relation = SnowflakeRelation.create(
         database="my_db",
         schema="my_schema",
         identifier="my_table",
-        type=RelationType.Table,
+        type=SnowflakeRelationType.Table,
     )
     assert relation.renameable_relations == frozenset(
         {
-            RelationType.Table,
+            SnowflakeRelationType.Table,
+            SnowflakeRelationType.View,
         }
     )
