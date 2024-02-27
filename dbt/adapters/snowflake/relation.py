def1d47db3a590f1485a0f79ad115d0b53b2c4ec
Mila Page <versusfacit@users.noreply.github.com>
2024-02-27 01:44:01 -0800

    Alter formatting.

--- dbt/adapters/snowflake/relation.py
+++ dbt/adapters/snowflake/relation.py
@@ -19,9 +19,7 @@ from dbt.adapters.snowflake.relation_configs import (
 @dataclass(frozen=True, eq=False, repr=False)
 class SnowflakeRelation(BaseRelation):
     type: Optional[SnowflakeRelationType] = None  # type: ignore
-    quote_policy: SnowflakeQuotePolicy = field(
-        default_factory=lambda: SnowflakeQuotePolicy()
-    )
+    quote_policy: SnowflakeQuotePolicy = field(default_factory=lambda: SnowflakeQuotePolicy())
 
     renameable_relations: FrozenSet[SnowflakeRelationType] = field(
         default_factory=lambda: frozenset(
@@ -61,24 +59,17 @@ class SnowflakeRelation(BaseRelation):
         existing_dynamic_table = SnowflakeDynamicTableConfig.from_relation_results(
             relation_results
         )
-        new_dynamic_table = SnowflakeDynamicTableConfig.from_relation_config(
-            relation_config
-        )
+        new_dynamic_table = SnowflakeDynamicTableConfig.from_relation_config(relation_config)
 
         config_change_collection = SnowflakeDynamicTableConfigChangeset()
 
         if new_dynamic_table.target_lag != existing_dynamic_table.target_lag:
-            config_change_collection.target_lag = (
-                SnowflakeDynamicTableTargetLagConfigChange(
-                    action=RelationConfigChangeAction.alter,
-                    context=new_dynamic_table.target_lag,
-                )
+            config_change_collection.target_lag = SnowflakeDynamicTableTargetLagConfigChange(
+                action=RelationConfigChangeAction.alter,
+                context=new_dynamic_table.target_lag,
             )
 
-        if (
-            new_dynamic_table.snowflake_warehouse
-            != existing_dynamic_table.snowflake_warehouse
-        ):
+        if new_dynamic_table.snowflake_warehouse != existing_dynamic_table.snowflake_warehouse:
             config_change_collection.snowflake_warehouse = (
                 SnowflakeDynamicTableWarehouseConfigChange(
                     action=RelationConfigChangeAction.alter,
