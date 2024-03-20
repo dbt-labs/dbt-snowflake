c50996711b796706a613dc261ab1460438fca1b5
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2024-03-20 16:05:18 -0500

    take into account uppercase

--- dbt/include/snowflake/macros/catalog.sql
+++ dbt/include/snowflake/macros/catalog.sql
@@ -42,7 +42,7 @@
         table_schema as "table_schema",
         table_name as "table_name",
         case
-            when is_dynamic = 'yes' and table_type = 'BASE TABLE' THEN 'DYNAMIC TABLE'
+            when is_dynamic = 'YES' and table_type = 'BASE TABLE' THEN 'DYNAMIC TABLE'
             else table_type
         end as "table_type",
         comment as "table_comment",
