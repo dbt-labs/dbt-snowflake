5459fbf0aa1bb40e800b89200442fcab47504157
Doug Beatty <44704949+dbeatty10@users.noreply.github.com>
2024-05-13 17:33:38 -0600

    Cross-database `date` macro (#1011)
    * Cross-database `date` macro
    
    * Temporary dev requirements for testing
    
    * Update changelog entry
    
    * Revert "Temporary dev requirements for testing"
    
    This reverts commit 7c4424dffd80e0dc93c3228bae13773903f58595.
    
    ---------
    
    Co-authored-by: Colin Rogers <111200756+colin-rogers-dbt@users.noreply.github.com>

--- tests/functional/adapter/utils/test_utils.py
+++ tests/functional/adapter/utils/test_utils.py
@@ -6,6 +6,7 @@ from dbt.tests.adapter.utils.test_bool_or import BaseBoolOr
 from dbt.tests.adapter.utils.test_cast_bool_to_text import BaseCastBoolToText
 from dbt.tests.adapter.utils.test_concat import BaseConcat
 from dbt.tests.adapter.utils.test_current_timestamp import BaseCurrentTimestampAware
+from dbt.tests.adapter.utils.test_date import BaseDate
 from dbt.tests.adapter.utils.test_dateadd import BaseDateAdd
 from dbt.tests.adapter.utils.test_datediff import BaseDateDiff
 from dbt.tests.adapter.utils.test_date_spine import BaseDateSpine
@@ -59,6 +60,10 @@ class TestConcat(BaseConcat):
 
 # Use either BaseCurrentTimestampAware or BaseCurrentTimestampNaive but not both
 class TestCurrentTimestamp(BaseCurrentTimestampAware):
+    pass
+
+
+class TestDate(BaseDate):
     pass
 
 
