f39276cc18b30c29bf9830c7b2ee549d2ee141c6
Mike Alfare <mike.alfare@dbtlabs.com>
2024-05-22 17:36:35 -0400

    jinja typo

--- dbt/include/snowflake/macros/relations/table/rename.sql
+++ dbt/include/snowflake/macros/relations/table/rename.sql
@@ -1,3 +1,3 @@
 {%- macro snowflake__get_rename_table_sql(relation, new_name) -%}
-    alter table {{ relation }} rename to {{ relation.incorporate(identifier=new_name}) }}
+    alter table {{ relation }} rename to {{ relation.incorporate(identifier=new_name) }}
 {%- endmacro -%}
--- dbt/include/snowflake/macros/relations/view/rename.sql
+++ dbt/include/snowflake/macros/relations/view/rename.sql
@@ -1,3 +1,3 @@
 {%- macro snowflake__get_rename_view_sql(relation, new_name) -%}
-    alter view {{ relation }} rename to {{ relation.incorporate(identifier=new_name}) }}
+    alter view {{ relation }} rename to {{ relation.incorporate(identifier=new_name) }}
 {%- endmacro -%}
