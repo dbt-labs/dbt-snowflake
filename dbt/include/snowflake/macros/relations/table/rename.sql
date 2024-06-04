81ff4de2b3fd1587a982782691e2ee1c075c4899
Mike Alfare <mike.alfare@dbtlabs.com>
2024-06-04 13:27:03 -0400

    override create_backup and rename_intermediate to use fully qualified names

--- dbt/include/snowflake/macros/relations/table/rename.sql
+++ dbt/include/snowflake/macros/relations/table/rename.sql
@@ -1,3 +1,13 @@
 {%- macro snowflake__get_rename_table_sql(relation, new_name) -%}
-    alter table {{ relation }} rename to {{ relation.incorporate(path={"identifier": new_name}) }}
+    /*
+    Rename or move a table to the new name.
+
+    Args:
+        relation: SnowflakeRelation - relation to be renamed
+        new_name: Union[str, SnowflakeRelation] - new name for `relation`
+            if providing a string, the default database/schema will be used if that string is just an identifier
+            if providing a SnowflakeRelation, `render` will be used to produce a fully qualified name
+    Returns: templated string
+    */
+    alter table {{ relation }} rename to {{ new_name }}
 {%- endmacro -%}
--- dbt/include/snowflake/macros/relations/view/rename.sql
+++ dbt/include/snowflake/macros/relations/view/rename.sql
@@ -1,3 +1,13 @@
 {%- macro snowflake__get_rename_view_sql(relation, new_name) -%}
-    alter view {{ relation }} rename to {{ relation.incorporate(path={"identifier": new_name}) }}
+    /*
+    Rename or move a view to the new name.
+
+    Args:
+        relation: SnowflakeRelation - relation to be renamed
+        new_name: Union[str, SnowflakeRelation] - new name for `relation`
+            if providing a string, the default database/schema will be used if that string is just an identifier
+            if providing a SnowflakeRelation, `render` will be used to produce a fully qualified name
+    Returns: templated string
+    */
+    alter view {{ relation }} rename to {{ new_name }}
 {%- endmacro -%}
