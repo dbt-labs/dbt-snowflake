8fa4f1d74b28501a599aa57e3bea66bb6edcedfc
Kshitij Aranke <kshitij.aranke@dbtlabs.com>
2024-06-05 16:49:27 +0100

    add broken test

--- setup.py
+++ setup.py
@@ -57,8 +57,8 @@ setup(
     packages=find_namespace_packages(include=["dbt", "dbt.*"]),
     include_package_data=True,
     install_requires=[
-        "dbt-common>=1.2.0,<2.0",
-        "dbt-adapters>=1.1.1,<2.0",
+        # "dbt-common>=1.2.0,<2.0",
+        "dbt-adapters @ git+https://github.com/dbt-labs/dbt-adapters.git@get_relation_metadata",
         "snowflake-connector-python[secure-local-storage]~=3.0",
         # add dbt-core to ensure backwards compatibility of installation, this is not a functional dependency
         "dbt-core>=1.8.0",
