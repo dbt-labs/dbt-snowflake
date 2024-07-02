a4042d498ee7127ed5011ecc86b3c521bc835e26
Mike Alfare <mike.alfare@dbtlabs.com>
2024-07-02 10:32:06 -0400

    add tests for all render instances

--- tests/functional/adapter/empty/_models.py
+++ tests/functional/adapter/empty/_models.py
@@ -6,6 +6,17 @@ my_id,my_value
 """.strip()
 
 
+SCHEMA = """
+version: 2
+
+seeds:
+  - name: my_seed
+    description: "This is my_seed"
+    columns:
+      - name: id
+        description: "This is my_seed.my_id"
+"""
+
 CONTROL = """
 select * from {{ ref("my_seed") }}
 """
@@ -13,12 +24,49 @@ select * from {{ ref("my_seed") }}
 
 GET_COLUMNS_IN_RELATION = """
 {{ config(materialized="table") }}
-select {{ adapter.get_columns_in_relation(ref("my_seed"))|length }} as get_columns_in_relation
+{% set columns = adapter.get_columns_in_relation(ref("my_seed")) %}
+select * from {{ ref("my_seed") }}
 """
 
 
 ALTER_COLUMN_TYPE = """
 {{ config(materialized="table") }}
 {{ alter_column_type(ref("my_seed"), "MY_VALUE", "string") }}
+select * from {{ ref("my_seed") }}
+"""
+
+
+ALTER_RELATION_COMMENT = """
+{{ config(
+    materialized="table",
+    persist_docs={"relations": True},
+) }}
+select * from {{ ref("my_seed") }}
+"""
+
+
+ALTER_COLUMN_COMMENT = """
+{{ config(
+    materialized="table",
+    persist_docs={"columns": True},
+) }}
+select * from {{ ref("my_seed") }}
+"""
+
+
+ALTER_RELATION_ADD_REMOVE_COLUMNS = """
+{{ config(materialized="table") }}
+{% set my_seed = adapter.Relation.create(this.database, this.schema, "my_seed", "table") %}
+{% set my_column = api.Column("my_column", "varchar") %}
+{% do alter_relation_add_remove_columns(my_seed, [my_column], none) %}
+{% do alter_relation_add_remove_columns(my_seed, none, [my_column]) %}
+select * from {{ ref("my_seed") }}
+"""
+
+
+TRUNCATE_RELATION = """
+{{ config(materialized="table") }}
+{% set my_seed = adapter.Relation.create(this.database, this.schema, "my_seed", "table") %}
+{{ truncate_relation(my_seed) }}
 select * from {{ ref("my_seed") }}
 """
--- tests/functional/adapter/empty/test_empty.py
+++ tests/functional/adapter/empty/test_empty.py
@@ -21,9 +21,14 @@ class TestMetadataWithEmptyFlag:
     @pytest.fixture(scope="class")
     def models(self):
         return {
+            "schema.yml": _models.SCHEMA,
             "control.sql": _models.CONTROL,
             "get_columns_in_relation.sql": _models.GET_COLUMNS_IN_RELATION,
             "alter_column_type.sql": _models.ALTER_COLUMN_TYPE,
+            "alter_relation_comment.sql": _models.ALTER_RELATION_COMMENT,
+            "alter_column_comment.sql": _models.ALTER_COLUMN_COMMENT,
+            "alter_relation_add_remove_columns.sql": _models.ALTER_RELATION_ADD_REMOVE_COLUMNS,
+            "truncate_relation.sql": _models.TRUNCATE_RELATION,
         }
 
     @pytest.fixture(scope="class", autouse=True)
@@ -36,6 +41,10 @@ class TestMetadataWithEmptyFlag:
             "control",
             "get_columns_in_relation",
             "alter_column_type",
+            "alter_relation_comment",
+            "alter_column_comment",
+            "alter_relation_add_remove_columns",
+            "truncate_relation",
         ],
     )
     def test_run(self, project, model):
