9a585662d140dbcfe75c3bc28b7c56e8aec9b54c
Colin <colin.rogers@dbtlabs.com>
2024-07-10 16:51:25 -0700

    test dynamic snowflake warehouse assignment via macro

--- tests/functional/adapter/dynamic_table_tests/files.py
+++ tests/functional/adapter/dynamic_table_tests/files.py
@@ -31,3 +31,37 @@ MY_DYNAMIC_TABLE = """
 ) }}
 select * from {{ ref('my_seed') }}
 """
+
+MY_DYNAMIC_TABLE = """
+{{ config(
+    materialized='dynamic_table',
+    snowflake_warehouse='DBT_TESTING',
+    target_lag='2        minutes',
+    refresh_mode='INCREMENTAL',
+) }}
+select * from {{ ref('my_seed') }}
+"""
+
+MY_DYNAMIC_TABLE_WITH_DYNAMIC_WAREHOUSE = """
+{{ config(
+    materialized = 'dynamic_table',
+    snowflake_warehouse = get_warehouse('xsmall'),
+    target_lag = 'downstream',
+    on_configuration_change = 'apply',
+) }}
+
+select * from {{ ref('my_seed') }}
+
+"""
+
+GET_WAREHOUSE_MACRO = """
+{% macro get_warehouse(size) %}
+    {% set warehouses = {
+        'xsmall': 'DBT_TESTING',
+        'small': 'DBT_TESTING',
+        'medium': 'DBT_TESTING',
+        'large': 'DBT_TESTING',
+    } %}
+    {{ return(warehouses[size]) }}
+{% endmacro %}
+"""
