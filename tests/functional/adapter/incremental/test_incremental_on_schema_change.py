ef093a0a277b2155d2b127159cb34b0d64eeb4b7
McKnight-42 <matthew.mcknight@dbtlabs.com>
2024-07-16 13:36:20 -0500

    pull in new basclass and run test

--- tests/functional/adapter/incremental/test_incremental_on_schema_change.py
+++ tests/functional/adapter/incremental/test_incremental_on_schema_change.py
@@ -1,7 +1,12 @@
 from dbt.tests.adapter.incremental.test_incremental_on_schema_change import (
     BaseIncrementalOnSchemaChange,
+    BaseIncrementalCaseSenstivityOnSchemaChange,
 )
 
 
 class TestIncrementalOnSchemaChange(BaseIncrementalOnSchemaChange):
+    pass
+
+
+class TestIncrementalCaseSenstivityOnSchemaChange(BaseIncrementalCaseSenstivityOnSchemaChange):
     pass
