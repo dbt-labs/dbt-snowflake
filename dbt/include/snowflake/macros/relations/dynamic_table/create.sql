a3b13b85fa290c90bd8da9b1177adc729284a8c2
Mila Page <versusfacit@users.noreply.github.com>
2024-08-28 22:10:43 -0700

    revert dynamic table changes.

--- dbt/include/snowflake/macros/relations/dynamic_table/create.sql
+++ dbt/include/snowflake/macros/relations/dynamic_table/create.sql
@@ -1,30 +1,16 @@
 {% macro snowflake__get_create_dynamic_table_as_sql(relation, sql) -%}
 
     {%- set dynamic_table = relation.from_config(config.model) -%}
-    {%- set iceberg = config.get('object_format', default='') == 'iceberg' -%}
 
-    {# Configure for extended Object Format #}
-    {% if iceberg -%}
-      {%- set object_format = 'iceberg' -%}
-    {%- else -%}
-      {%- set object_format = '' -%}
-    {%- endif -%}
-
-    create dynamic {{ object_format }} table {{ relation }}
+    create dynamic table {{ relation }}
         target_lag = '{{ dynamic_table.target_lag }}'
         warehouse = {{ dynamic_table.snowflake_warehouse }}
-        {%- if iceberg %}
-          external_volume = {{ config.get('external_volume') }}
-          catalog = 'snowflake'
-          base_location = {{ config.get('base_location') }}
-        {%- else -%}
-          {% if dynamic_table.refresh_mode %}
-          refresh_mode = {{ dynamic_table.refresh_mode }}
-          {% endif %}
-          {% if dynamic_table.initialize %}
-          initialize = {{ dynamic_table.initialize }}
-          {% endif %}
-        {%- endif %}
+        {% if dynamic_table.refresh_mode %}
+        refresh_mode = {{ dynamic_table.refresh_mode }}
+        {% endif %}
+        {% if dynamic_table.initialize %}
+        initialize = {{ dynamic_table.initialize }}
+        {% endif %}
         as (
             {{ sql }}
         )
