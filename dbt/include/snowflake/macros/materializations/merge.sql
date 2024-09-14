d4a2b66a489aec7c92f507d4a3b86e94df26803a
Michelle Ark <michelle.ark@dbtlabs.com>
2024-09-13 22:05:43 -0400

    tidy up snowflake__get_incremental_microbatch_sql

--- dbt/include/snowflake/macros/materializations/merge.sql
+++ dbt/include/snowflake/macros/materializations/merge.sql
@@ -53,12 +53,12 @@
 {% macro snowflake__get_incremental_microbatch_sql(arg_dict) %}
     {% set incremental_predicates = [] if arg_dict.get('incremental_predicates') is none else  arg_dict.get('incremental_predicates') %}
     {#-- Add additional incremental_predicates if it is safe to do so --#}
-    {% if model.config.event_time -%}
-        {% if model.config.event_time_start -%}
-            {% do incremental_predicates.append("DBT_INTERNAL_DEST" ~ "." ~ model.config.event_time ~ " >= " ~ model.__dbt_internal_microbatch_event_time_start) %}
+    {% if config.get("event_time") -%}
+        {% if config.get("__dbt_internal_microbatch_event_time_start") -%}
+            {% do incremental_predicates.append("DBT_INTERNAL_DEST" ~ "." ~ config.event_time ~ " >= " ~ config.__dbt_internal_microbatch_event_time_start) %}
         {% endif %}
-        {% if model.config.event_time_start -%}
-            {% do incremental_predicates.append("DBT_INTERNAL_DEST" ~ "." ~ model.config.event_time ~ " < " ~ model.__dbt_internal_microbatch_event_time_start) %}
+        {% if model.config.__dbt_internal_microbatch_event_time_end -%}
+            {% do incremental_predicates.append("DBT_INTERNAL_DEST" ~ "." ~ model.config.event_time ~ " < " ~ model.__dbt_internal_microbatch_event_time_end %}
         {% endif %}
     {% endif %}
     {% do arg_dict.update({'incremental_predicates': incremental_predicates}) %}
