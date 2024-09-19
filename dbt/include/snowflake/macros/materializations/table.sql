ccd3ebeb12f0905d1cf15f0ca5bd64cb736c426a
Mike Alfare <mike.alfare@dbtlabs.com>
2024-09-18 20:27:36 -0400

    remove a macro that only contains a docstring

--- dbt/include/snowflake/macros/materializations/table.sql
+++ dbt/include/snowflake/macros/materializations/table.sql
@@ -72,17 +72,3 @@ def main(session):
     materialize(session, df, dbt.this)
     return "OK"
 {% endmacro %}
-
-{% macro py_script_comment()%}
-# To run this in snowsight, you need to select entry point to be main
-# And you may have to modify the return type to text to get the result back
-# def main(session):
-#     dbt = dbtObj(session.table)
-#     df = model(dbt, session)
-#     return df.collect()
-
-# to run this in local notebook, you need to create a session connected to Snowpark
-# then you can do the following to run model
-# dbt = dbtObj(session.table)
-# df = model(dbt, session)
-{%endmacro%}
