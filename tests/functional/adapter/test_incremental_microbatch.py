583ec5eb2686720b5715b0121c4c7d79a3e319d3
Michelle Ark <MichelleArk@users.noreply.github.com>
2024-09-23 22:57:17 +0100

    Add required 'begin' config for testing microbatch models (#1189)

--- tests/functional/adapter/test_incremental_microbatch.py
+++ tests/functional/adapter/test_incremental_microbatch.py
@@ -6,7 +6,7 @@ from dbt.tests.adapter.incremental.test_incremental_microbatch import (
 
 # No requirement for a unique_id for snowflake microbatch!
 _microbatch_model_no_unique_id_sql = """
-{{ config(materialized='incremental', incremental_strategy='microbatch', event_time='event_time', batch_size='day') }}
+{{ config(materialized='incremental', incremental_strategy='microbatch', event_time='event_time', batch_size='day', begin=modules.datetime.datetime(2020, 1, 1, 0, 0, 0)) }}
 select * from {{ ref('input_model') }}
 """
 
