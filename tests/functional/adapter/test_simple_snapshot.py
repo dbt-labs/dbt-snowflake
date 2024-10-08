2c0eb6ac2b8e4c2832fff2a0e21b7b0f6e5d9f15
Gerda Shank <gerda@dbtlabs.com>
2024-10-08 19:42:02 -0400

    Another typo

--- tests/functional/adapter/test_simple_snapshot.py
+++ tests/functional/adapter/test_simple_snapshot.py
@@ -1,6 +1,6 @@
 from dbt.tests.adapter.simple_snapshot.test_snapshot import BaseSnapshotCheck, BaseSimpleSnapshot
 
-from dbt.tests.adapter.simple_snapshot.test_various_snapshot_configs import (
+from dbt.tests.adapter.simple_snapshot.test_various_configs import (
     BaseSnapshotColumnNames,
     BaseSnapshotColumnNamesFromDbtProject,
     BaseSnapshotInvalidColumnNames,
