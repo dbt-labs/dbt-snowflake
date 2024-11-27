8196b632db21c341bf87e8bec2ff39a6817a9634
Michelle Ark <michelle.ark@dbtlabs.com>
2024-11-27 16:19:14 -0500

    use timestamp with tz (utc) in snowflake__get_incremental_microbatch_sql incremental_predicates

--- tests/functional/adapter/test_incremental_microbatch.py
+++ tests/functional/adapter/test_incremental_microbatch.py
@@ -3,10 +3,20 @@ from dbt.tests.adapter.incremental.test_incremental_microbatch import (
     BaseMicrobatch,
 )
 
+# Create input with UTC timestamps
+_input_model_sql = """
+{{ config(materialized='table', event_time='event_time') }}
+select 1 as id, to_timestamp_tz('2020-01-01 00:00:00-0') as event_time
+union all
+select 2 as id, to_timestamp_tz('2020-01-02 00:00:00-0') as event_time
+union all
+select 3 as id, to_timestamp_tz('2020-01-03 00:00:00-0') as event_time
+"""
+
 
 # No requirement for a unique_id for snowflake microbatch!
 _microbatch_model_no_unique_id_sql = """
-{{ config(materialized='incremental', incremental_strategy='microbatch', event_time='event_time', batch_size='day', begin=modules.datetime.datetime(2020, 1, 1, 0, 0, 0)) }}
+{{ config(materialized='incremental', incremental_strategy='microbatch', event_time='event_time', batch_size='day', begin=modules.datetime.datetime(2020, 1, 1, 0, 0, 0), pre_hook="alter session set timezone = 'America/Los_Angeles'") }}
 select * from {{ ref('input_model') }}
 """
 
@@ -15,6 +25,10 @@ class TestSnowflakeMicrobatch(BaseMicrobatch):
     @pytest.fixture(scope="class")
     def microbatch_model_sql(self) -> str:
         return _microbatch_model_no_unique_id_sql
+
+    @pytest.fixture(scope="class")
+    def input_model_sql(self) -> str:
+        return _input_model_sql
 
     @pytest.fixture(scope="class")
     def insert_two_rows_sql(self, project) -> str:
