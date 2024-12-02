238bae2809283186e37eb7a20c3d76c519d06169
Michelle Ark <michelle.ark@dbtlabs.com>
2024-12-02 10:12:38 -0500

    remove unnecessary pre-hook in microbatch functional test

--- tests/functional/adapter/test_incremental_microbatch.py
+++ tests/functional/adapter/test_incremental_microbatch.py
@@ -16,7 +16,7 @@ select 3 as id, to_timestamp_tz('2020-01-03 00:00:00-0') as event_time
 
 # No requirement for a unique_id for snowflake microbatch!
 _microbatch_model_no_unique_id_sql = """
-{{ config(materialized='incremental', incremental_strategy='microbatch', event_time='event_time', batch_size='day', begin=modules.datetime.datetime(2020, 1, 1, 0, 0, 0), pre_hook="alter session set timezone = 'America/Los_Angeles'") }}
+{{ config(materialized='incremental', incremental_strategy='microbatch', event_time='event_time', batch_size='day', begin=modules.datetime.datetime(2020, 1, 1, 0, 0, 0)) }}
 select * from {{ ref('input_model') }}
 """
 
