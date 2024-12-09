091c3bbd5d360df7d356f05e8e6153ece56b0c52
VersusFacit <versusfacit@users.noreply.github.com>
2024-12-09 12:06:55 -0800

    Add failing test.

--- tests/functional/relation_tests/dynamic_table_tests/test_basic.py
+++ tests/functional/relation_tests/dynamic_table_tests/test_basic.py
@@ -1,6 +1,6 @@
 import pytest
 
-from dbt.tests.util import run_dbt
+from dbt.tests.util import assert_message_in_logs, run_dbt, run_dbt_and_capture
 
 from tests.functional.relation_tests.dynamic_table_tests import models
 from tests.functional.utils import query_relation_type
@@ -46,3 +46,33 @@ class TestBasicIcebergOn(TestBasic):
     @pytest.fixture(scope="class")
     def project_config_update(self):
         return {"flags": {"enable_iceberg_materializations": True}}
+
+
+class TestAutoConfigDoesntRebuild:
+    DT_NAME = "my_dynamic_table"
+
+    @pytest.fixture(scope="class", autouse=True)
+    def seeds(self):
+        return {"my_seed.csv": models.SEED}
+
+    @pytest.fixture(scope="class", autouse=True)
+    def models(self):
+        yield {
+            f"{self.DT_NAME}.sql": models.AUTO_DYNAMIC_TABLE,
+        }
+
+    def test_auto_config_doesnt_rebuild(self, project):
+        model_qualified_name = f"{project.database}.{project.test_schema}.{self.DT_NAME}"
+
+        run_dbt(["seed"])
+        _, logs = run_dbt_and_capture(["--debug", "run", "--select", f"{self.DT_NAME}.sql"])
+        assert_message_in_logs(f"create dynamic table {model_qualified_name}", logs)
+        assert_message_in_logs("refresh_mode = AUTO", logs)
+
+        _, logs = run_dbt_and_capture(["--debug", "run", "--select", f"{self.DT_NAME}.sql"])
+        assert_message_in_logs(f"create dynamic table {model_qualified_name}", logs, False)
+        assert_message_in_logs("refresh_mode = AUTO", logs, False)
+        assert_message_in_logs(
+            f"No configuration changes were identified on on: `{model_qualified_name}`. Continuing.",
+            logs,
+        )
