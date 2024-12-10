bd0c4ab9b4876b3d582580c2bbbd3ab76989a52b
Mike Alfare <mike.alfare@dbtlabs.com>
2024-12-09 19:07:38 -0500

    fix dbt dependency updated; remove extra python setup call

--- .github/scripts/update_dev_dependency_branches.sh
+++ .github/scripts/update_dev_dependency_branches.sh
@@ -6,7 +6,7 @@ dbt_adapters_branch=$1
 dbt_core_branch=$2
 dbt_common_branch=$3
 target_req_file="hatch.toml"
-core_req_sed_pattern="s|dbt-core.git#subdirectory=core|dbt-core.git@${dbt_core_branch}#subdirectory=core|g"
+core_req_sed_pattern="s|dbt-core.git.*#subdirectory=core|dbt-core.git@${dbt_core_branch}#subdirectory=core|g"
 adapters_req_sed_pattern="s|dbt-adapters.git|dbt-adapters.git@${dbt_adapters_branch}|g"
 common_req_sed_pattern="s|dbt-common.git|dbt-common.git@${dbt_common_branch}|g"
 if [[ "$OSTYPE" == darwin* ]]; then
